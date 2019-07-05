752c2a9bce453ae6f3d3e01ce805fdad0d789f09
nimelehin <nimelehin@gmail.com>
2019-07-05 17:54:00 +0300

    display driver (scroll)

--- drivers/display/display.c
+++ drivers/display/display.c
@@ -7,59 +7,51 @@
 void print_char(char symbol, unsigned char color, int col, int row) {
     char* mem = VIDEO_MEMORY;
     unsigned short offset_types[2] = {get_cursor_offet(), get_offset(col, row)};
-    unsigned short offset;
-    offset = offset_types[(col_in_field(col) && row_in_field(row))]; 
+    unsigned short offset = offset_types[(col_in_field(col) && row_in_field(row))]; 
     mem[offset * 2] = symbol;
     mem[offset * 2 + 1] = color;
-    if (offset == MAX_COLS * MAX_ROWS) {
-        scroll(1); // scrool for one line;
+    if (offset + 1 >= MAX_COLS * MAX_ROWS) {
+        scroll(1); // scroll for 1 line
     } else {
         set_cursor_offset(offset+1);
     }
 }
 
-void printString(const char* bo[], unsigned char color, int col, int row) {
-    char* mem = VIDEO_MEMORY;
-    unsigned int position = row * MAX_COLS + col;
-    // TODO print string
+void print_string(const char* string, unsigned char color, int col, int row) {
+    int i = 0;
+    while (string[i] != 0) {
+        print_char(string[i++], color, col, row);
+        col = row = -1;
+    }
 }
 
 // clean screen
 
 void clean_screen() {
     char* mem = VIDEO_MEMORY;
-    for (int i = 0; i < MAX_COLS * MAX_ROWS; i+=2) {
+    for (int i = 0; i < 2 * MAX_COLS * MAX_ROWS; i+=2) {
         mem[i] = ' ';
         mem[i+1] = WHITE_ON_BLACK;
     }
-    set_cursor_offset(0, 0);
+    set_cursor_offset(0);
 }
 
 // scroll
 
 void scroll (unsigned char lines) {
     char* mem = VIDEO_MEMORY;
-    for (int i = lines; i < MAX_ROWS; i++){
-        for (int j = 0; j < MAX_COLS; j++) {
-            int offset = get_offset(j, i);
-            int offsetOld = get_offset(j, i - lines);
-            if (offset - offsetOld == 80) {
-                mem[1] = 'X';
-                mem[3] = 'z';
-                mem[1] = mem[3];
-            }
-            //mem[2 * offsetOld + 1] = mem[2 * offset + 1];
-            //mem[2 * offsetOld + 2] = mem[2 * offset + 2];
-        }
+    *mem = 'C';
+    unsigned int mem_offset = lines * MAX_COLS;
+    unsigned int bytes_2_del = 2*(MAX_COLS*MAX_ROWS-mem_offset);
+    char* src = (mem + mem_offset*2);
+    memcpy(src, mem, bytes_2_del);
+
+    for (int i = bytes_2_del; i < 2 * MAX_COLS * MAX_ROWS; i+=2) {
+        mem[i] = ' ';
+        mem[i + 1] = WHITE_ON_BLACK;
     }
-     //set_cursor_offset(MAX_COLS*MAX_ROWS - offset/2);
-    for (int i = 0; i < MAX_COLS; i++){
-        for (int j = MAX_ROWS - lines; j < MAX_ROWS; j++){
-            int offset = get_offset(i, j);
-            mem[2 * offset + 1] = ' ';
-            mem[2 * offset + 2] =  WHITE_ON_BLACK;
-        }
-    }
+
+    set_cursor_offset(bytes_2_del/2);
 }
 
 // cursor
@@ -85,6 +77,3 @@ int get_cursor_offet() {
 int get_offset(int col, int row) { return row * MAX_COLS + col; }
 bool col_in_field(int col) { return (0 <= col && col <= MAX_COLS); }
 bool row_in_field(int row) { return (0 <= row && row <= MAX_ROWS); }
-
-int get_offset_row(int offset) { return offset / (2 * MAX_COLS); }
-int get_offset_col(int offset) { return (offset - (get_offset_row(offset)*2*MAX_COLS))/2; }
