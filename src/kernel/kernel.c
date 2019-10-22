c4594fa50c85d20c131f31bb7752c728aabaffca
nimelehin <nimelehin@gmail.com>
2019-10-22 10:01:28 +0300

    program run

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -23,6 +23,30 @@ void main() {
     ata_write(&ata0m, "test", 4);
     ata_flush(&ata0m);
     ata_read(&ata0m);
+
+    // test.s program
+    char *ram = 0x100000;
+    ram[0] = 0x66;
+    ram[1] = 0xb8;
+    ram[2] = 0x03;
+    ram[3] = 0x00;
+    ram[4] = 0x00;
+    ram[5] = 0x00;
+    ram[6] = 0x66;
+    ram[7] = 0xbb;
+    ram[8] = 0x06;
+    ram[9] = 0x00;
+    ram[10] = 0x00;
+    ram[11] = 0x00;
+    ram[12] = 0xcd;
+    ram[13] = 0x00;
+    ram[14] = 0xeb;
+    ram[15] = 0xfe;
+    
+    asm volatile("mov $0x100000, %eax");
+    asm volatile("mov %eax, %ebp");
+    asm volatile("jmp %eax");
+    
     
     asm volatile("int $0"); // test interrupts
     while (1) {}
---
+++ test.s
@@ -0,0 +1,5 @@
+mov eax, 3
+mov ebx, 6
+int 0x01
+lol:
+jmp lol
