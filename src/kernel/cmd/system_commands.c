b537a64e5210777caa8cd77794e77e4ebfda1afe
nimelehin <nimelehin@gmail.com>
2019-11-23 12:05:41 +0300

    Added programs (wr, echo, mkdir)
    Added new programs (wr, echo, mkdir). Now these programs can use
    params (argc, argv)
    
    Names of commands will be changed

--- src/kernel/cmd/system_commands.c
+++ src/kernel/cmd/system_commands.c
@@ -1,20 +1,51 @@
 #include <cmd/system_commands.h>
 #include <fs/vfs.h>
+#include <mem/malloc.h>
 
 void _syscmd_init_vfs();
 
 // imitation of LS app
 void _syscmd_ls(int argc, char *argv[]) {
-    vfs_lookup_dir("/");
+    vfs_element_t vfs_buf[16]; // 16 tmp value (will be replaced with std value)
+    char str_buf[VFS_MAX_FILENAME + VFS_MAX_FILENAME_EXT + 2];
+    uint32_t n = vfs_lookup_dir("/", vfs_buf);
+    for (int i = 0; i < n; i++) {
+        uint32_t fname_len = strlen(vfs_buf[i].filename);
+        uint32_t fext_len = strlen(vfs_buf[i].filename_ext);
+
+        memcpy(str_buf, vfs_buf[i].filename, fname_len);
+
+        if (vfs_buf[i].attributes >= 0x10) {
+            str_buf[fname_len] = '/';
+        } else {
+            str_buf[fname_len] = '.';
+            memcpy(str_buf+fname_len+1, vfs_buf[i].filename_ext, fext_len);
+        }
+
+        str_buf[fname_len + fext_len + 1] = '\0';
+        printf(str_buf);
+        printf("\n");
+    }
 }
 
 // imitation of mkdir app
 void _syscmd_mkdir(int argc, char *argv[]) {
-    printf(argv[1]);
     vfs_create_dir("/", argv[1]);
+}
+
+void _syscmd_echo(int argc, char *argv[]) {
+    char *dd = vfs_read_file("/", argv[1], 0, -1);
+    printf(dd);
+    kfree(dd);
+}
+
+void _syscmd_write(int argc, char *argv[]) {
+    vfs_write_file("/", argv[1], "hello", 5);
 }
 
 void syscmd_init() {
     cmd_register("ls", _syscmd_ls);
     cmd_register("mkdir", _syscmd_mkdir);
+    cmd_register("echo", _syscmd_echo);
+    cmd_register("wr", _syscmd_write);
 }
