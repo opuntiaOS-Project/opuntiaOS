0eb5b4d0f6eb04cdc71674d6618b44ccc14046e9
nimelehin <nimelehin@gmail.com>
2020-03-03 21:53:29 +0300

    Tasking: run_proc() is split into functions

--- src/kernel/tasking/tasking.c
+++ src/kernel/tasking/tasking.c
@@ -10,6 +10,7 @@
 extern irq_return();
 extern switch_contexts(context_t **old, context_t *new);
 
+// switching the page dir and tss to the current proc
 void switchuvm(proc_t *p) {
     gdt[SEG_TSS] = SEG_BG(SEGTSS_TYPE, &tss, sizeof(tss)-1, 0);
     tss.esp0 = p->kstack + VMM_PAGE_SIZE;
@@ -20,22 +21,16 @@ void switchuvm(proc_t *p) {
     vmm_switch_pdir(p->pdir);
 }
 
-void jmper() {
-    // jump to trap frame
+// used to jump to trapend
+// the jump will start the process
+void jumper() {
     return;
 }
 
-// Start proccess
-// TODO split into functions
-void run_proc() {
-    nxt_proc = 0;
-    proc_t *p = &proc[nxt_proc];
-    uint8_t *code = vfs_read_file("/", "main.sys", 0, -1);
-    vfs_element_t fstat = vfs_get_file_info("/", "main.sys");
-
-    p->pdir = vmm_new_user_pdir();   
-    
-    vmm_copy_program_data(p->pdir, code, fstat.file_size);
+// alocating proc and kernel stack of the proc
+void allocate_proc(proc_t *p) {
+    // creating new page dir
+    p->pdir = vmm_new_user_pdir();
 
     // allocating kernel stack
     p->kstack = kmalloc(VMM_PAGE_SIZE);
@@ -47,53 +42,49 @@ void run_proc() {
     sp -= sizeof(*p->context);
     p->context = (context_t*)sp;
     memset(p->context, 0, sizeof(*p->context));
-    p->context->eip = (uint32_t)jmper;
-
+    p->context->eip = (uint32_t)jumper;
     memset(p->tf, 0, sizeof(*p->tf));
+}
+
+// Start init proccess
+// All others processes will fork
+void run_proc() {
+    nxt_proc = 0;
+    proc_t *p = &proc[nxt_proc];
+    
+    uint8_t *code = vfs_read_file("/", "main.sys", 0, -1);
+    vfs_element_t fstat = vfs_get_file_info("/", "main.sys");
+
+    allocate_proc(p);
+    vmm_copy_program_data(p->pdir, code, fstat.file_size);
     p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
     p->tf->ds = (SEG_UDATA << 3) | DPL_USER;
     p->tf->es = p->tf->ds;
     p->tf->ss = p->tf->ds;
     p->tf->eflags = FL_IF;
-    p->tf->ebp = VMM_PAGE_SIZE - 1;
-    p->tf->esp = VMM_PAGE_SIZE - 1;
+    p->tf->ebp = VMM_PAGE_SIZE;
+    p->tf->esp = VMM_PAGE_SIZE;
     p->tf->eip = 0;
 
-    context_t tmpr;
-    context_t *tmp_ptr = &tmpr;
+    // stub context
+    context_t stub_cntx;
+    context_t *stub_cntx_ptr = &stub_cntx;
 
     switchuvm(p);
-    switch_contexts(&tmp_ptr, p->context);
+    switch_contexts(&stub_cntx_ptr, p->context);
 }
 
 // TODO tmp solution to launch the second proc
 // Will be deleted 
 void set_proc2() {
-
-    // setup_scheduler();
-
     nxt_proc = 1;
     proc_t *p = &proc[nxt_proc];
     char *code = vfs_read_file("/", "init2.sys", 0, -1);
     vfs_element_t fstat = vfs_get_file_info("/", "init2.sys");
     
-    p->pdir = vmm_new_user_pdir();   
-    
+    allocate_proc(p);
     vmm_copy_program_data(p->pdir, code, fstat.file_size);
 
-    // allocating kernel stack
-    p->kstack = kmalloc(VMM_PAGE_SIZE);
-    char *sp = p->kstack + VMM_PAGE_SIZE;
-    sp -= sizeof(*p->tf);
-    p->tf = (trapframe_t *)sp;
-    sp -= 4;
-    *(uint32_t*)sp = (uint32_t)irq_return;
-    sp -= sizeof(*p->context);
-    p->context = (context_t*)sp;
-    memset(p->context, 0, sizeof(*p->context));
-    p->context->eip = (uint32_t)jmper;
-
-    memset(p->tf, 0, sizeof(*p->tf));
     p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
     p->tf->ds = (SEG_UDATA << 3) | DPL_USER;
     p->tf->es = p->tf->ds;
