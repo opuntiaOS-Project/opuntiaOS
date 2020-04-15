0ec34acb8f8dda119d3f15a65a6497a1fd239b22
nimelehin <nimelehin@gmail.com>
2020-04-15 19:53:27 +0300

    Kernel: Start ext2 & VFS rework

--- include/fs/ext2/ext2.h
+++ include/fs/ext2/ext2.h
@@ -51,9 +51,83 @@ typedef struct {
     uint8_t unused[1024-206];
 } superblock_t;
 
+#define GROUP_LEN (sizeof(group_desc_t))
+typedef struct {
+    uint32_t block_bitmap;
+    uint32_t inode_bitmap;
+    uint32_t inode_table;
+    uint16_t free_blocks_count;
+    uint16_t free_inodes_count;
+    uint16_t used_dirs_count;
+    uint16_t pad;
+    uint8_t  reserved[12];
+} group_desc_t;
+
+#define EXT2_S_IFSOCK 0xC000
+#define EXT2_S_IFLNK 0xA000
+#define EXT2_S_IFREG 0x8000
+#define EXT2_S_IFBLK 0x6000
+#define EXT2_S_IFDIR 0x4000
+#define EXT2_S_IFCHR 0x2000
+#define EXT2_S_IFIFO 0x1000
+
+#define EXT2_S_ISUID 0x0800
+#define EXT2_S_ISGID 0x0400
+#define EXT2_S_ISVTX 0x0200
+
+#define EXT2_S_IRUSR 0x0100
+#define EXT2_S_IWUSR 0x0080
+#define EXT2_S_IXUSR 0x0040
+#define EXT2_S_IRGRP 0x0020
+#define EXT2_S_IWGRP 0x0010
+#define EXT2_S_IXGRP 0x0008
+#define EXT2_S_IROTH 0x0004
+#define EXT2_S_IWOTH 0x0002
+#define EXT2_S_IXOTH 0x0001
+
+#define INODE_LEN (sizeof(inode_t))
+#define INODES_RESERVED 11
+typedef struct {
+    uint16_t mode;
+    uint16_t uid;
+    uint32_t size;
+    uint32_t atime;
+    uint32_t ctime;
+    uint32_t mtime;
+    uint32_t dtime;
+    uint16_t gid;
+    uint16_t links_count;
+    uint32_t blocks;
+    uint32_t flags;
+    uint32_t osd1;
+    uint32_t block[15];
+    uint32_t generation;
+    uint32_t file_acl;
+    uint32_t dir_acl;
+    uint32_t faddr;
+    uint32_t osd2[3];
+} inode_t;
+
+    
+typedef struct {
+    uint32_t inode;
+    uint16_t rec_len;
+    uint8_t name_len;
+    uint8_t file_type;
+    char* name; // TODO maybe not used?
+} dir_entry_t;
+
 
 void ext2_install();
 bool ext2_recognize_drive(vfs_device_t *dev);
 void ext2_stub();
+
+int ext2_read_inode(vfs_device_t *dev, uint32_t inode_id, inode_t *inode);
+int ext2_has_in_dir(vfs_device_t *dev, uint32_t block_idx, const char *path, uint32_t *found_inode_idx);
+int ext2_scan_dir(vfs_device_t *dev, inode_t inode, const char *path, uint32_t *res_inode_idx);
+int ext2_get_inode_idx(vfs_device_t *dev, const char *path, uint32_t *file_inode);
+int ext2_open(vfs_device_t *dev, const char *path, file_descriptor_t *fd);
+int ext2_read(vfs_device_t *dev, file_descriptor_t *fd, uint8_t* buf, uint32_t start, uint32_t len);
+int ext2_write(vfs_device_t *dev, file_descriptor_t *fd, uint8_t* buf, uint32_t start, uint32_t len);
 
 #endif // __oneOS__FS__EXT2__EXT2_H
--- src/kernel/fs/ext2/ext2.c
+++ src/kernel/fs/ext2/ext2.c
@@ -1,16 +1,71 @@
 #include <fs/ext2/ext2.h>
 #include <mem/malloc.h>
 
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define ext2_get_superblock() _ext2_superblocks[dev->dev.id]
+#define ext2_get_group_tables() _ext2_group_tables[dev->dev.id]
+
 static superblock_t *_ext2_superblocks[MAX_DEVICES_COUNT];
+static group_desc_t *_ext2_group_tables[MAX_DEVICES_COUNT];
 
 driver_desc_t _ext2_driver_info();
-void _ext2_read_superblock();
 
-void _ext2_read_superblock(vfs_device_t *dev, superblock_t *buf) {
-    void (*rd)(device_t *d, uint32_t s, uint8_t *r) =
+/* TOOLS */
+void _ext2_read_from_dev(vfs_device_t *dev, uint8_t *buf, uint32_t start, uint32_t len);
+void _ext2_write_to_dev(vfs_device_t *dev, uint8_t *buf, uint32_t start, uint32_t len);
+uint32_t _ext2_get_block_len(vfs_device_t *dev);
+uint32_t _ext2_get_groups_cnt(vfs_device_t *dev);
+uint32_t _ext2_get_block_offset(vfs_device_t *dev, uint32_t block_idx);
+
+
+void _ext2_read_from_dev(vfs_device_t *dev, uint8_t *buf, uint32_t start, uint32_t len) {
+    void (*read)(device_t *d, uint32_t s, uint8_t *r) =
         drivers[dev->dev.driver_id].driver_desc.functions[DRIVER_STORAGE_READ];
-    rd(&dev->dev, 2, (uint8_t*)buf);
-    // rd(&dev->dev, 1024+512, ((uint8_t*)buf)+512);
+    int already_read = 0;
+    uint8_t tmp_buf[512];
+    while (len != 0) {
+        uint32_t sector = start / 512;
+        uint32_t start_offset = start % 512;
+        read(&dev->dev, sector, tmp_buf);
+        for (int i = 0; i < MIN(512 - start_offset, len); i++) {
+            buf[already_read++] = tmp_buf[start_offset + i];
+        }
+        len -= MIN(512 - start_offset, len);
+        start += MIN(512 - start_offset, len); 
+    }
+}
+
+void _ext2_write_to_dev(vfs_device_t *dev, uint8_t *buf, uint32_t start, uint32_t len) {
+    void (*read)(device_t *d, uint32_t s, uint8_t *r) =
+        drivers[dev->dev.driver_id].driver_desc.functions[DRIVER_STORAGE_READ];
+    void (*write)(device_t *d, uint32_t s, uint8_t *r, uint32_t siz) =
+        drivers[dev->dev.driver_id].driver_desc.functions[DRIVER_STORAGE_WRITE];
+    int already_written = 0;
+    uint8_t tmp_buf[512];
+    while (len != 0) {
+        uint32_t sector = start / 512;
+        uint32_t start_offset = start % 512;
+        if (start_offset != 0 || len < 512) {
+            printf("READ SECTOR\n");
+            read(&dev->dev, sector, tmp_buf);
+        }
+        printf("OFFSET"); printd(start_offset); printf("\n");
+        for (int i = 0; i < MIN(512 - start_offset, len); i++) {
+            tmp_buf[start_offset + i] = buf[already_written++];
+        }
+        write(&dev->dev, sector, tmp_buf, 512);
+        len -= MIN(512 - start_offset, len);
+        start += MIN(512 - start_offset, len);
+    }
+}
+
+
+uint32_t _ext2_get_block_len(vfs_device_t *dev) {
+    return (1 << (ext2_get_superblock()->log_block_size + 10));
+}
+
+uint32_t _ext2_get_block_offset(vfs_device_t *dev, uint32_t block_idx) {
+    return SUPERBLOCK_START + (block_idx - 1) * _ext2_get_block_len(dev);
 }
 
 driver_desc_t _ext2_driver_info() {
@@ -21,6 +76,9 @@ driver_desc_t _ext2_driver_info() {
     fs_desc.is_device_needed = false;
     fs_desc.is_driver_needed = false;
     fs_desc.functions[DRIVER_FILE_SYSTEM_RECOGNIZE] = ext2_recognize_drive;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_OPEN] = ext2_open;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_READ] = ext2_read;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_WRITE] = ext2_write;
     fs_desc.functions[DRIVER_FILE_SYSTEM_CREATE_DIR] = ext2_stub;
     fs_desc.functions[DRIVER_FILE_SYSTEM_LOOKUP_DIR] = ext2_stub;
     fs_desc.functions[DRIVER_FILE_SYSTEM_REMOVE_DIR] = ext2_stub;
@@ -35,12 +93,11 @@ void ext2_install() {
     driver_install(_ext2_driver_info());
 }
 
-// TODO better to return int
+// TODO return int
 bool ext2_recognize_drive(vfs_device_t *dev) {
-    superblock_t *superblock = (superblock_t*)kmalloc(1024);
-    _ext2_read_superblock(dev, superblock);
-    // printh(superblock->magic);
-    // while (1) {}
+    superblock_t *superblock = (superblock_t*)kmalloc(SUPERBLOCK_LEN);
+    _ext2_read_from_dev(dev, (uint8_t*)superblock, SUPERBLOCK_START, SUPERBLOCK_LEN);
+    
     if (superblock->magic != 0xEF53) {
         return false;
     }
@@ -50,12 +107,228 @@ bool ext2_recognize_drive(vfs_device_t *dev) {
 
     _ext2_superblocks[dev->dev.id] = superblock;
 
-    printf("EXT2\n");
-    while (1) {}
-    // start setup process
+    // FIXME: for now we consider that we have at max 5 groups
+    uint32_t group_table_len = 5 * GROUP_LEN;
+    group_desc_t *group_table = (group_desc_t*)kmalloc(group_table_len);
+    _ext2_read_from_dev(dev, (uint8_t*)group_table, _ext2_get_block_offset(dev, 2), group_table_len);
+
+    _ext2_group_tables[dev->dev.id] = group_table;
+
     return true;
 }
 
 void ext2_stub() {
     printf("EXT2\n");
+}
+
+int ext2_read_inode(vfs_device_t *dev, uint32_t inode_id, inode_t *inode) {
+    uint32_t inodes_per_group = ext2_get_superblock()->inodes_per_group;
+    uint32_t holder_group = (inode_id - 1) / inodes_per_group;
+    uint32_t pos_inside_group = (inode_id - 1) % inodes_per_group;
+    uint32_t inode_start = _ext2_get_block_offset(dev, _ext2_group_tables[dev->dev.id][holder_group].inode_table) + (pos_inside_group * INODE_LEN);
+    _ext2_read_from_dev(dev, (uint8_t*)inode, inode_start, INODE_LEN);
+    return 0; 
+}
+
+int ext2_has_in_dir(vfs_device_t *dev, uint32_t block_idx, const char *path, uint32_t *found_inode_idx) {
+    if (block_idx == 0) {
+        return -1;
+    }
+
+    uint8_t *tmp_buf = (uint8_t*)kmalloc(_ext2_get_block_len(dev));
+    _ext2_read_from_dev(dev, tmp_buf, _ext2_get_block_offset(dev, block_idx), _ext2_get_block_len(dev));
+    dir_entry_t* start_of_entry = (dir_entry_t*)tmp_buf;
+    for (;;) {
+        if (start_of_entry->inode == 0) {
+            kfree(tmp_buf);
+            return -1;
+        }
+        // checking name of this entry
+        bool is_name_correct = true;
+        for (int i = 0; i < start_of_entry->name_len; i++) {
+            is_name_correct &= (path[i] == *((char*)start_of_entry+8+i));
+            
+        }
+        is_name_correct &= (path[start_of_entry->name_len] == '\0' || path[start_of_entry->name_len] == '/');
+        
+        if (is_name_correct) {
+            *found_inode_idx = start_of_entry->inode;
+            kfree(tmp_buf);
+            return 0;
+        }
+        
+        start_of_entry = (dir_entry_t*)((uint32_t)start_of_entry + start_of_entry->rec_len);
+        if ((uint32_t)start_of_entry >= (uint32_t)tmp_buf + _ext2_get_block_len(dev)) {
+            kfree(tmp_buf);
+            return -1;
+        }
+    }
+    kfree(tmp_buf);
+    return -2;
+}
+
+int ext2_scan_dir(vfs_device_t *dev, inode_t inode, const char *path, uint32_t *res_inode_idx) {
+    uint32_t nxt_inode_index;
+    uint32_t path_offset = 0;
+    for (;;) {
+new_inode:
+        for (int i = 0; i < 12; i++) {
+            if (inode.block[i] != 0) {
+                if (ext2_has_in_dir(dev, inode.block[i], &path[path_offset], &nxt_inode_index) == 0) {
+                    for (; path[path_offset] != '/'; path_offset++) {
+                        if (path[path_offset] == '\0') {
+                            *res_inode_idx = nxt_inode_index;
+                            return 0;
+                        }
+                    }
+                    path_offset++;
+                    ext2_read_inode(dev, nxt_inode_index, &inode);
+                    goto new_inode;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int ext2_get_inode_idx(vfs_device_t *dev, const char *path, uint32_t *file_inode) {
+    inode_t root_inode;
+    ext2_read_inode(dev, 2, &root_inode);
+
+    int res = -1;
+    if (path[0] == '/') {
+        res = ext2_scan_dir(dev, root_inode, &path[1], file_inode);
+    } else {
+        res = ext2_scan_dir(dev, root_inode, path, file_inode);
+    }
+    
+    return res;
+}
+
+int ext2_open(vfs_device_t *dev, const char *path, file_descriptor_t *fd) {
+    uint32_t inode_idx;
+    inode_t inode;
+    // while (1) {}
+    if (ext2_get_inode_idx(dev, path, &inode_idx) < 0) {
+        // perror("[Ext2]: can't find inode");
+        return -1;
+    }
+    // while (1) {}
+    if (ext2_read_inode(dev, inode_idx, &inode) < 0) {
+        // perror("[Ext2]: can't load inode");
+        return -1;
+    }
+    fd->mode = inode.mode;
+    fd->atime = inode.atime;
+    fd->ctime = inode.ctime;
+    fd->mtime = inode.mtime;
+    fd->mode = inode.mode;
+    fd->size = inode.size;
+    fd->dev_id = dev->dev.id;
+    fd->inode_idx = inode_idx;
+    return 0;
+}
+
+uint32_t _ext2_get_block_of_file_lev0(vfs_device_t *dev, uint32_t cur_block, uint32_t file_block_index) {
+    uint32_t offset = file_block_index;
+    uint32_t res;
+    _ext2_read_from_dev(dev, (uint8_t*)&res, _ext2_get_block_offset(dev, cur_block) + offset * 4, 4);
+    return res;
+}
+
+uint32_t _ext2_get_block_of_file_lev1(vfs_device_t *dev, uint32_t cur_block, uint32_t file_block_index) {
+    uint32_t lev_contain = _ext2_get_block_len(dev) / 4;
+    uint32_t offset = file_block_index / lev_contain;
+    uint32_t offset_inner = file_block_index % lev_contain;
+    uint32_t res;
+    _ext2_read_from_dev(dev, (uint8_t*)&res, _ext2_get_block_offset(dev, cur_block) + offset * 4, 4);
+    return _ext2_get_block_of_file_lev0(dev, res, offset_inner);
+}
+
+uint32_t _ext2_get_block_of_file_lev2(vfs_device_t *dev, uint32_t cur_block, uint32_t file_block_index) {
+    uint32_t block_len = _ext2_get_block_len(dev) / 4;
+    uint32_t lev_contain = block_len * block_len;
+    uint32_t offset = file_block_index / lev_contain;
+    uint32_t offset_inner = file_block_index % lev_contain;
+    uint32_t res;
+    _ext2_read_from_dev(dev, (uint8_t*)&res, _ext2_get_block_offset(dev, cur_block) + offset * 4, 4);
+    return _ext2_get_block_of_file_lev1(dev, res, offset_inner);
+}
+
+uint32_t _ext2_get_block_of_file_lev3(vfs_device_t *dev, uint32_t cur_block, uint32_t file_block_index) {
+     uint32_t block_len = _ext2_get_block_len(dev) / 4;
+    uint32_t lev_contain = block_len * block_len * block_len;
+    uint32_t offset = file_block_index / lev_contain;
+    uint32_t offset_inner = file_block_index % lev_contain;
+    uint32_t res;
+    _ext2_read_from_dev(dev, (uint8_t*)&res, _ext2_get_block_offset(dev, cur_block) + offset * 4, 4);
+    return _ext2_get_block_of_file_lev2(dev, res, offset_inner);
+}
+
+// TODO think of more effecient way
+uint32_t _ext2_get_block_of_file(vfs_device_t *dev, inode_t *inode, uint32_t file_block_index) {
+    uint32_t block_len = _ext2_get_block_len(dev) / 4;
+    if (file_block_index < 12) {
+        return inode->block[file_block_index];
+    } else if (file_block_index < 12 + block_len) { // single indirect
+        return _ext2_get_block_of_file_lev0(dev, inode->block[12], file_block_index - 12);
+    } else if (file_block_index < 12 + block_len + block_len * block_len) { // double indirect
+        return _ext2_get_block_of_file_lev1(dev, inode->block[13], file_block_index - 12 - block_len);
+    } else { // triple indirect
+        return _ext2_get_block_of_file_lev0(dev, inode->block[14], file_block_index - (12 + block_len + block_len * block_len));
+    }
+}
+
+
+int ext2_read(vfs_device_t *dev, file_descriptor_t *fd, uint8_t* buf, uint32_t start, uint32_t len) {
+    inode_t inode;
+    ext2_read_inode(dev, fd->inode_idx, &inode);
+
+    const uint32_t block_len = _ext2_get_block_len(dev);
+    uint32_t start_block_index = start / block_len;
+    uint32_t end_block_index = (start + len) / block_len;
+    uint32_t read_offset = start % block_len;
+    uint32_t already_read = 0; 
+
+    for (uint32_t block_index = start_block_index; block_index <= end_block_index; block_index++) {
+        uint32_t data_block_index = _ext2_get_block_of_file(dev, &inode, block_index);
+        _ext2_read_from_dev(dev, buf+already_read, _ext2_get_block_offset(dev, data_block_index) + read_offset, MIN(len, block_len - read_offset));
+        printf("Reading "); printd(already_read); printf("\n");
+        len -= MIN(len, block_len - read_offset);
+        already_read += MIN(len, block_len - read_offset);
+        read_offset = 0;
+    }
+    if (len != 0) {
+        return -1;
+    }
+
+    printf("RET 0\n");
+    return 0;
+}
+
+int ext2_write(vfs_device_t *dev, file_descriptor_t *fd, uint8_t* buf, uint32_t start, uint32_t len) {
+    inode_t inode;
+    ext2_read_inode(dev, fd->inode_idx, &inode);
+
+    const uint32_t block_len = _ext2_get_block_len(dev);
+    uint32_t start_block_index = start / block_len;
+    uint32_t end_block_index = (start + len) / block_len;
+    uint32_t write_offset = start % block_len;
+    uint32_t already_written = start % block_len;
+    
+    for (uint32_t block_index = start_block_index; block_index <= end_block_index; block_index++) {
+        uint32_t data_block_index = _ext2_get_block_of_file(dev, &inode, block_index);
+        _ext2_write_to_dev(dev, buf+already_written, _ext2_get_block_offset(dev, data_block_index) + write_offset, MIN(len, block_len - write_offset));
+        printf("Writing "); printd (MIN(len, block_len - write_offset)); printf("\n");
+        len -= MIN(len, block_len - write_offset);
+        already_written += MIN(len, block_len - write_offset);
+        write_offset = 0;
+
+    }
+    if (len != 0) {
+        return -1;
+    }
+
+    printf("RET 0\n");
+    return 0;
 }
