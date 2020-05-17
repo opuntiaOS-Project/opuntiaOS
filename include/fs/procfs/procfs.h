ebd2c6a23d1959187ffe77ec5b68c69d685157fd
nimelehin <nimelehin@gmail.com>
2020-05-17 10:37:20 +0300

    ProcFS: Add procfs to test mount()

---
+++ include/fs/procfs/procfs.h
@@ -0,0 +1,8 @@
+#ifndef __oneOS__FS__PROCFS__PROCFS_H
+#define __oneOS__FS__PROCFS__PROCFS_H
+
+#include <types.h>
+
+void procfs_install();
+
+#endif // __oneOS__FS__PROCFS__PROCFS_H
---
+++ src/kernel/fs/procfs/procfs.c
@@ -0,0 +1,65 @@
+#include <fs/vfs.h>
+#include <mem/kmalloc.h>
+
+static int test_proc_num = 8;
+
+int procfs_read_inode(dentry_t* dentry)
+{
+    dentry->inode->mode = EXT2_S_IFDIR;
+    return 0;
+}
+
+int procfs_write_inode(dentry_t* dentry)
+{
+    return 0;
+}
+
+fsdata_t procfs_data(dentry_t* dentry)
+{
+    fsdata_t fsdata;
+    fsdata.sb = 0;
+    fsdata.gt = 0;
+    return fsdata;
+}
+
+int procfs_getdirent(dentry_t* dir, uint32_t* offset, dirent_t* res)
+{
+    if (*offset >= test_proc_num) {
+        return -1;
+    }
+
+    res->inode_indx = *offset + 2;
+    char val = (*offset + (int)'0');
+    memcpy(res->name, "Proc", 4);
+    memcpy(res->name+4, &val, 1);
+
+    (*offset)++;
+    return 0;
+}
+
+driver_desc_t _procfs_driver_info()
+{
+    driver_desc_t fs_desc;
+    fs_desc.type = DRIVER_FILE_SYSTEM;
+    fs_desc.auto_start = false;
+    fs_desc.is_device_driver = false;
+    fs_desc.is_device_needed = false;
+    fs_desc.is_driver_needed = false;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_RECOGNIZE] = 0;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_READ] = 0;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_WRITE] = 0;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_MKDIR] = 0;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_EJECT_DEVICE] = 0;
+
+    fs_desc.functions[DRIVER_FILE_SYSTEM_READ_INODE] = procfs_read_inode;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_WRITE_INODE] = procfs_write_inode;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_GET_FSDATA] = procfs_data;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_LOOKUP] = 0;
+    fs_desc.functions[DRIVER_FILE_SYSTEM_GETDIRENT] = procfs_getdirent;
+    return fs_desc;
+}
+
+void procfs_install()
+{
+    driver_install(_procfs_driver_info());
+}
