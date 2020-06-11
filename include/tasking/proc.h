6ed22b0a9fcff970700179733ce0694b4014ea02
nimelehin <nimelehin@gmail.com>
2020-06-11 20:36:34 +0300

    Proc: Add zone functions.
    Add basic zone functions, will be improved later :^)

--- include/tasking/proc.h
+++ include/tasking/proc.h
@@ -9,6 +9,7 @@
 #ifndef __oneOS__X86__TASKING__PROC_H
 #define __oneOS__X86__TASKING__PROC_H
 
+#include <algo/dynamic_array.h>
 #include <fs/vfs.h>
 #include <mem/vmm/vmm.h>
 #include <mem/vmm/zoner.h>
@@ -27,6 +28,26 @@ typedef struct {
     uint32_t eip;
 } __attribute__((packed)) context_t;
 
+enum PROC_ZONE_FLAGS {
+    PROC_ZONE_EXEC = 0x1,
+    PROC_ZONE_WRITE = 0x2,
+};
+
+enum PROC_ZONE_TYPES {
+    PROC_ZONE_CODE = 0x1,
+    PROC_ZONE_DATA = 0x2,
+    PROC_ZONE_STACK = 0x4,
+    PROC_ZONE_MMAP = 0x8,
+};
+
+struct proc_zone {
+    uint32_t start;
+    uint32_t len;
+    uint32_t type;
+    uint32_t flags;
+};
+typedef struct proc_zone proc_zone_t;
+
 typedef struct {
     uint32_t sz;
     pdirectory_t* pdir;
@@ -36,6 +57,8 @@ typedef struct {
     context_t* context; // context of kernel's registers
     trapframe_t* tf;
 
+    dynamic_array_t zones;
+
     dentry_t* cwd;
     file_descriptor_t* fds;
 
@@ -43,7 +66,6 @@ typedef struct {
     uint32_t pending_signals_mask;
     void* signal_handlers[SIGNALS_CNT];
 } __attribute__((packed)) proc_t;
-
 
 int proc_prepare(proc_t* p);
 int proc_free(proc_t* p);
@@ -54,5 +76,13 @@ int proc_free(proc_t* p);
 
 file_descriptor_t* proc_get_free_fd(proc_t* proc);
 file_descriptor_t* proc_get_fd(proc_t* proc, int index);
+
+
+/**
+ * PROC ZONING
+ */
+
+proc_zone_t* proc_new_zone(proc_t* proc, uint32_t start, uint32_t len);
+proc_zone_t* proc_new_random_zone(proc_t* proc, int size);
 
 #endif // __oneOS__X86__TASKING__PROC_H
