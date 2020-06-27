27a58acfbdeefeaba5b9bb513dab37fe34871759
nimelehin <nimelehin@gmail.com>
2020-06-27 11:43:13 +0300

    Proc: Update zoner's functions
    Add functions which can allocate zones starting from the beginning and
    starting from the end.

--- src/kernel/tasking/proc.c
+++ src/kernel/tasking/proc.c
@@ -49,7 +49,7 @@ int proc_free(proc_t* p)
     if (p->pid == 0) {
         return -1;
     }
-    
+
     /* closing opend fds */
     for (int i = 0; i < MAX_OPENED_FILES; i++) {
         if (p->fds[i].dentry) {
@@ -90,22 +90,19 @@ file_descriptor_t* proc_get_fd(proc_t* proc, int index)
  */
 
 static inline bool _proc_zones_intersect(uint32_t start1, uint32_t size1, uint32_t start2, uint32_t size2)
-{   
+{
     uint32_t end1 = start1 + size1 - 1;
     uint32_t end2 = start2 + size2 - 1;
-    return  (start1 <= start2 && start2 <= end1)    ||
-            (start1 <= end2 && end2 <= end1)        ||
-            (start2 <= start1 && start1 <= end2)    ||
-            (start2 <= end1 && end1 <= end2);
+    return (start1 <= start2 && start2 <= end1) || (start1 <= end2 && end2 <= end1) || (start2 <= start1 && start1 <= end2) || (start2 <= end1 && end1 <= end2);
 }
 
-static inline bool _proc_can_add_zone(proc_t* proc, proc_zone_t* zone)
+static inline bool _proc_can_add_zone(proc_t* proc, uint32_t start, uint32_t len)
 {
     uint32_t zones_count = proc->zones.size;
 
     for (uint32_t i = 0; i < zones_count; i++) {
         proc_zone_t* zone = (proc_zone_t*)dynamic_array_get(&proc->zones, i);
-        if (_proc_zones_intersect(zone->start, zone->len, zone->start, zone->len)) {
+        if (_proc_zones_intersect(start, len, zone->start, zone->len)) {
             return false;
         }
     }
@@ -119,36 +116,70 @@ proc_zone_t* proc_new_zone(proc_t* proc, uint32_t start, uint32_t len)
     new_zone.start = start;
     new_zone.len = len;
 
-    if (_proc_can_add_zone(proc, &new_zone)) {
+    if (_proc_can_add_zone(proc, start, len)) {
         if (dynamic_array_push(&proc->zones, &new_zone) != 0) {
             return 0;
         }
-        return (proc_zone_t*)dynamic_array_get(&proc->zones, proc->zones.size-1);
+        return (proc_zone_t*)dynamic_array_get(&proc->zones, proc->zones.size - 1);
     }
 
     return 0;
 }
 
-// FIXME: We think them sorted.
-proc_zone_t* proc_new_random_zone(proc_t* proc, int size)
+/* FIXME: Think of more efficient way */
+proc_zone_t* proc_new_random_zone(proc_t* proc, uint32_t len)
 {
-    uint32_t prev_end = 0;
     uint32_t zones_count = proc->zones.size;
+
+    /* Check if we can put it at the beginning */
+    proc_zone_t* ret = proc_new_zone(proc, 0, len);
+    if (ret) {
+        return ret;
+    }
+
+    uint32_t min_start = 0xffffffff;
 
     for (uint32_t i = 0; i < zones_count; i++) {
         proc_zone_t* zone = (proc_zone_t*)dynamic_array_get(&proc->zones, i);
-        if (prev_end + size - 1 < zone->start) {
-            goto found;
+        if (_proc_can_add_zone(proc, zone->start + zone->len, len)) {
+            if (min_start > zone->start + zone->len) {
+                min_start = zone->start + zone->len;
+            }
         }
-        prev_end = zone->start + zone->len;
     }
 
-    if (prev_end + size - 1 < KERNEL_BASE) {
-        goto found;
+    if (min_start == 0xffffffff) {
+        return 0;
     }
 
-    return 0;
+    return proc_new_zone(proc, min_start, len);
+}
 
-found:
-    return proc_new_zone(proc, prev_end, size);
+/* FIXME: Think of more efficient way */
+proc_zone_t* proc_new_random_zone_backward(proc_t* proc, uint32_t len)
+{
+    uint32_t zones_count = proc->zones.size;
+
+    /* Check if we can put it at the end */
+    proc_zone_t* ret = proc_new_zone(proc, KERNEL_BASE - len, len);
+    if (ret) {
+        return ret;
+    }
+
+    uint32_t max_end = 0;
+
+    for (uint32_t i = 0; i < zones_count; i++) {
+        proc_zone_t* zone = (proc_zone_t*)dynamic_array_get(&proc->zones, i);
+        if (_proc_can_add_zone(proc, zone->start - len, len)) {
+            if (max_end < zone->start) {
+                max_end = zone->start;
+            }
+        }
+    }
+
+    if (max_end == 0) {
+        return 0;
+    }
+
+    return proc_new_zone(proc, max_end - len, len);
 }
