bf36bc6d9c546e5fb5d4d4db71dae3ebe51dd928
nimelehin <nimelehin@gmail.com>
2020-06-27 19:26:53 +0300

    LibSystem: Add write syscall

--- libs/libsystem/syscalls.h
+++ libs/libsystem/syscalls.h
@@ -1,4 +1,20 @@
+#ifndef __oneOS__libsystem__SYSCALLS_H
+#define __oneOS__libsystem__SYSCALLS_H
+
+#include "types.h"
+
 void print(int value);
+
+enum OPEN_MODE {
+    O_RDONLY,
+    O_WRONLY,
+    O_RDWR
+};
 int open(const char *pathname, int flags);
+int read(int fd, char *buf, size_t count);
+int write(int fd, const void *buf, size_t count);
+
 int raise(int signo);
 int sigaction(int signo, void* callback);
+
+#endif /* __oneOS__libsystem__SYSCALLS_H */
