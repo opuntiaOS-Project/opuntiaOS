4609023e68ca7b37c508fa098fa483bf7994d4d1
nimelehin <nimelehin@gmail.com>
2020-07-18 15:39:29 +0300

    Utils: Add strcmp and strncmp

--- include/utils.h
+++ include/utils.h
@@ -7,5 +7,7 @@ void htos(uint32_t hex, char str[]);
 void dtos(uint32_t dec, char str[]);
 void reverse(char s[]);
 uint32_t strlen(const char *s);
+int strcmp(const char* a, const char* b);
+int strncmp(const char* a, const char* b, uint32_t num);
 
 #endif
--- src/kernel/utils/utils.c
+++ src/kernel/utils/utils.c
@@ -1,50 +1,91 @@
 #include <utils.h>
 
-void htos(uint32_t hex, char str[]) {
+void htos(uint32_t hex, char str[])
+{
     int i = 0;
     if (hex == 0) {
         str[0] = '0';
         i = 1;
     }
-    while(hex != 0) {
+    while (hex != 0) {
         int num = hex % 16;
         hex /= 16;
         if (num < 10) {
-            str[i++] = num+'0';
+            str[i++] = num + '0';
         } else {
-            str[i++] = num+'a'-10;
+            str[i++] = num + 'a' - 10;
         }
     }
     str[i] = '\0';
     reverse(str);
 }
 
-void dtos(uint32_t dec, char str[]) {
+void dtos(uint32_t dec, char str[])
+{
     int i = 0;
     if (dec == 0) {
         str[0] = '0';
         i = 1;
     }
-    while(dec != 0) {
+    while (dec != 0) {
         int num = dec % 10;
         dec /= 10;
-        str[i++] = num+'0';
+        str[i++] = num + '0';
     }
     str[i] = '\0';
     reverse(str);
 }
 
-void reverse(char s[]) {
+void reverse(char s[])
+{
     int c, i, j;
-    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
         c = s[i];
         s[i] = s[j];
         s[j] = c;
     }
 }
 
-uint32_t strlen(const char *s) {
+uint32_t strlen(const char* s)
+{
     uint32_t i = 0;
-    while (s[i] != '\0') ++i;
+    while (s[i] != '\0')
+        ++i;
     return i;
+}
+
+int strcmp(const char* a, const char* b)
+{
+    while (*a == *b && *a != 0 && *b != 0) {
+        a++;
+        b++;
+    }
+
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+int strncmp(const char* a, const char* b, uint32_t num)
+{
+    while (*a == *b && *a != 0 && *b != 0 && num) {
+        a++;
+        b++;
+        num--;
+    }
+
+    if (!num) {
+        return 0;
+    } else if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    }
+
+    return 0;
 }
