9e186aea0671c412555468e03ab30453bcac10f9
Plunkerusr <slava.pach@gmail.com>
2020-08-30 16:39:28 +0300

    LibC: Implement malloc

---
+++ libs/libsystem/string.h
@@ -0,0 +1,13 @@
+#ifndef __oneOS__LibC__STRING_H
+#define __oneOS__LibC__STRING_H
+
+#include "types.h"
+
+extern int errno;
+
+void memset(uint8_t* dest, uint8_t fll, uint32_t nbytes);
+void memcpy(uint8_t* dest, const uint8_t* src, uint32_t nbytes);
+int memcmp(const uint8_t* src1, const uint8_t* src2, uint32_t nbytes);
+void memccpy(uint8_t* dest, const uint8_t* src, uint8_t stop, uint32_t nbytes);
+
+#endif // __oneOS__LibC__STRING_H
