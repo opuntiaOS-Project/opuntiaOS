d1bde2d49529a544144a6f11963c1de5666cd761
nimelehin <nimelehin@gmail.com>
2020-10-04 18:55:27 +0300

    Drivers: Add mouse to devfs

--- include/drivers/mouse.h
+++ include/drivers/mouse.h
@@ -5,6 +5,14 @@
 #include <drivers/ata.h>
 #include <x86/idt.h>
 
+/* The mouse packet should be aligned to 4 bytes */
+struct mouse_packet {
+    int16_t x_offset;
+    int16_t y_offset;
+    uint32_t button_states;
+};
+typedef struct mouse_packet mouse_packet_t;
+
 bool mouse_install();
 
 #endif // __oneOS__DRIVERS__M_H
--- src/kernel/drivers/mouse.c
+++ src/kernel/drivers/mouse.c
@@ -1,15 +1,55 @@
+#include <algo/ringbuffer.h>
 #include <drivers/display.h>
 #include <drivers/driver_manager.h>
+#include <drivers/mouse.h>
+#include <fs/devfs/devfs.h>
+#include <log.h>
 #include <types.h>
+#include <utils/kassert.h>
 #include <x86/idt.h>
 #include <x86/port.h>
-#include <utils/kassert.h>
 
 // #define MOUSE_DRIVER_DEBUG
 
-static int mouse_x = 0, mouse_y = 0;
+static ringbuffer_t mouse_buffer;
 
 void mouse_run();
+
+static bool _mouse_can_read(dentry_t* dentry, uint32_t start)
+{
+    return ringbuffer_space_to_read(&mouse_buffer) >= 1;
+}
+
+static int _mouse_read(dentry_t* dentry, uint8_t* buf, uint32_t start, uint32_t len)
+{
+    uint32_t leno = ringbuffer_space_to_read(&mouse_buffer);
+    if (leno > len) {
+        leno = len;
+    }
+    int res = ringbuffer_read(&mouse_buffer, buf, leno);
+    return leno;
+}
+
+static void _mouse_recieve_notification(uint32_t msg, uint32_t param)
+{
+    if (msg == DM_NOTIFICATION_DEVFS_READY) {
+        dentry_t* mp;
+        if (vfs_resolve_path("/dev", &mp) < 0) {
+            kpanic("Can't init mouse in /dev");
+        }
+
+        file_ops_t fops;
+        fops.can_read = _mouse_can_read;
+        fops.can_write = 0;
+        fops.read = _mouse_read;
+        fops.write = 0;
+        fops.ioctl = 0;
+        fops.mmap = 0;
+        devfs_inode_t* res = devfs_register(mp, "mouse", 5, 0, &fops);
+
+        dentry_put(mp);
+    }
+}
 
 static driver_desc_t _mouse_driver_info()
 {
@@ -19,7 +59,7 @@ static driver_desc_t _mouse_driver_info()
     desc.is_device_driver = false;
     desc.is_device_needed = false;
     desc.is_driver_needed = false;
-    desc.functions[DRIVER_NOTIFICATION] = 0;
+    desc.functions[DRIVER_NOTIFICATION] = _mouse_recieve_notification;
     desc.functions[DRIVER_INPUT_SYSTEMS_ADD_DEVICE] = mouse_run;
     desc.functions[DRIVER_INPUT_SYSTEMS_GET_LAST_KEY] = 0;
     desc.functions[DRIVER_INPUT_SYSTEMS_DISCARD_LAST_KEY] = 0;
@@ -83,28 +123,38 @@ void mouse_handler()
     uint8_t is_right_button_pressed = (resp >> 1) & 1;
     uint8_t is_left_button_pressed = (resp >> 0) & 1;
 
-    if (x_sign) {
-        mouse_x += (int)xm;
-    } else {
-        mouse_x -= (int)xm;
+    mouse_packet_t packet;
+    packet.x_offset = xm;
+    packet.y_offset = ym;
+    packet.button_states = 0;
+
+    if (packet.x_offset && x_sign) {
+        packet.x_offset -= 0x100;
+    }
+    if (packet.y_offset && y_sign) {
+        packet.y_offset -= 0x100;
+    }
+    if (x_overflow || y_overflow) {
+        packet.x_offset = 0;
+        packet.y_offset = 0;
     }
 
-    if (y_sign) {
-        mouse_y += (int)ym;
-    } else {
-        mouse_y -= (int)ym;
-    }
+    packet.button_states |= (is_left_button_pressed);
+    packet.button_states |= (is_right_button_pressed << 1);
+    packet.button_states |= (is_mid_button_pressed << 2);
+
+    ringbuffer_write(&mouse_buffer, (uint8_t*)&packet, sizeof(mouse_packet_t));
 
 #ifdef MOUSE_DRIVER_DEBUG
-    if (mouse_x < 0) {
-        kprintf("-%d ", -mouse_x);
+    if (packet.x_offset < 0) {
+        log("-%d ", -packet.x_offset);
     } else {
-        kprintf("%d ", mouse_x);
+        log("%d ", packet.x_offset);
     }
-    if (my < 0) {
-        kprintf("-%d\n", -mouse_y);
+    if (packet.y_offset < 0) {
+        log("-%d\n", -packet.y_offset);
     } else {
-        kprintf("%d\n", mouse_y);
+        log("%d\n", packet.y_offset);
     }
 #endif /* MOUSE_DRIVER_DEBUG */
 }
@@ -116,6 +166,8 @@ void mouse_run()
     _mouse_send_cmd(0xF6);
     _mouse_send_cmd(0xF4);
     set_irq_handler(IRQ12, mouse_handler);
+
+    mouse_buffer = ringbuffer_create_std();
 }
 
 bool mouse_install()
