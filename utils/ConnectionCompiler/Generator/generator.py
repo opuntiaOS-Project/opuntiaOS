7b1e1cedb3295de5f9d3ca35aae2f37d169d531f
nimelehin <nimelehin@gmail.com>
2020-10-20 19:57:40 +0300

    Utils: Fix compiletion of c++ code

--- utils/ConnectionCompiler/Generator/generator.py
+++ utils/ConnectionCompiler/Generator/generator.py
@@ -72,10 +72,18 @@ class Generator:
         self.out("};")
         self.out("")
 
+    def decoder_create_vars(self, messages, offset = 0):
+        var_names = set()
+        for (name,params) in messages.items():
+            for i in params:
+                if 'var_{0}'.format(i[1]) not in var_names:
+                    self.out("{0} var_{1};".format(i[0], i[1]), offset)
+                    var_names.add('var_{0}'.format(i[1]))
+
+
     def decoder_decode_message(self, msg, offset = 0):
         params_str = ""
         for i in msg.params:
-            self.out("{0} var_{1};".format(i[0], i[1]), offset)
             params_str += "var_{0}, ".format(i[1])
 
         if len(msg.params) > 0:
@@ -91,11 +99,15 @@ class Generator:
         self.out("UniquePtr<Message> decode(const char* buf, size_t size, size_t& decoded_msg_len) override", 1)
         self.out("{", 1)
         self.out("int msg_id, decoder_magic;", 2)
+        self.out("size_t saved_dml = decoded_msg_len;", 2)
         self.out("Encoder::decode(buf, decoded_msg_len, decoder_magic);", 2)
         self.out("Encoder::decode(buf, decoded_msg_len, msg_id);", 2)
         self.out("if (magic() != decoder_magic) {", 2)
+        self.out("decoded_msg_len = saved_dml;", 3)
         self.out("return nullptr;", 3)
         self.out("}", 2)
+
+        self.decoder_create_vars(decoder.messages, 2)
 
         unique_msg_id = 1
         self.out("", 2)
@@ -106,6 +118,7 @@ class Generator:
             unique_msg_id += 1
             
         self.out("default:", 2)
+        self.out("decoded_msg_len = saved_dml;", 3)
         self.out("return nullptr;", 3)
         self.out("}", 2)
         self.out("}", 1)
@@ -122,8 +135,7 @@ class Generator:
         self.out("", 2)
         self.out("switch(msg.id()) {", 2)
         for (name,params) in decoder.messages.items():
-            ret = decoder.functions.get(name, None)
-            if ret is not None:
+            if name in decoder.functions:
                 self.out("case {0}:".format(unique_msg_id), 2)
                 self.out("return handle(static_cast<const {0}&>(msg));".format(name), 3)
             
@@ -137,8 +149,7 @@ class Generator:
 
     def decoder_create_virtual_handle(self, decoder):
         for (accept,ret) in decoder.functions.items():
-            if ret is not None:
-                self.out("virtual UniquePtr<Message> handle(const {0}& msg) {{ return nullptr; }}".format(accept), 1)
+            self.out("virtual UniquePtr<Message> handle(const {0}& msg) {{ return nullptr; }}".format(accept), 1)
 
     def generate_decoder(self, decoder):
         self.out("class {0} : public MessageDecoder {{".format(decoder.name))
@@ -156,6 +167,7 @@ class Generator:
         self.out("#include <libipc/Encoder.h>")
         self.out("#include <libipc/ClientConnection.h>")
         self.out("#include <libipc/ServerConnection.h>")
+        self.out("#include <libg/Rect.h>")
         self.out("#include <malloc.h>")
         self.out("")
 
