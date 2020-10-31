07c3b470ba3d2b09b49b579298e62608c29165fc
nimelehin <nimelehin@gmail.com>
2020-10-31 18:13:38 +0300

    LibG: More constructors for PixelBitmap

--- libs/libg/PixelBitmap.cpp
+++ libs/libg/PixelBitmap.cpp
@@ -7,32 +7,60 @@
  */
 
 #include "PixelBitmap.h"
-#include <libcxx/syscalls.h>
+#include <std/Dbg.h>
 
 namespace LG {
 
 PixelBitmap::PixelBitmap(Color* buffer, size_t width, size_t height)
     : m_data(buffer)
-    , m_width(width)
-    , m_height(height)
+    , m_bounds(0, 0, width, height)
     , m_should_free(false)
 {
 }
 
 PixelBitmap::PixelBitmap(size_t width, size_t height)
-    : m_width(width)
-    , m_height(height)
-    , m_data((Color*)malloc(sizeof(Color) * m_width * m_height))
+    : m_bounds(0, 0, width, height)
+    , m_data((Color*)malloc(sizeof(Color) * width * height))
     , m_should_free(true)
 {
 }
 
+// FIXME: Possible mem leak.
+PixelBitmap::PixelBitmap(PixelBitmap& bitmap)
+    : m_data(nullptr)
+    , m_bounds(bitmap.m_bounds)
+    , m_should_free(bitmap.m_should_free)
+{
+    if (m_should_free) {
+        size_t len = width() * height() * sizeof(Color);
+        m_data = (Color*)malloc(len);
+        memcpy((uint8_t*)m_data, (uint8_t*)bitmap.m_data, len);
+    } else {
+        m_data = bitmap.m_data;
+    }
+}
+
 PixelBitmap::PixelBitmap(PixelBitmap&& moved_bitmap)
     : m_data(moved_bitmap.m_data)
-    , m_width(moved_bitmap.m_width)
-    , m_height(moved_bitmap.m_height)
+    , m_bounds(moved_bitmap.m_bounds)
     , m_should_free(moved_bitmap.m_should_free)
 {
+    moved_bitmap.m_data = nullptr;
+    moved_bitmap.bounds().set_width(0);
+    moved_bitmap.bounds().set_height(0);
+    moved_bitmap.m_should_free = false;
+}
+
+void PixelBitmap::resize(size_t width, size_t height)
+{
+    if (m_should_free) {
+        free(m_data);
+    }
+
+    bounds().set_width(width);
+    bounds().set_height(height);
+    m_data = (Color*)malloc(sizeof(Color) * width * height);
+    m_should_free = true;
 }
 
 }
--- libs/libg/PixelBitmap.h
+++ libs/libg/PixelBitmap.h
@@ -11,6 +11,7 @@
 #include "Color.h"
 #include "Rect.h"
 #include <libcxx/malloc.h>
+#include <libcxx/std/Dbg.h>
 #include <libcxx/sys/types.h>
 
 namespace LG {
@@ -20,6 +21,7 @@ public:
     PixelBitmap() = default;
     PixelBitmap(size_t width, size_t height);
     PixelBitmap(Color* buffer, size_t width, size_t height);
+    PixelBitmap(PixelBitmap& bitmap);
     PixelBitmap(PixelBitmap&& moved_bitmap) noexcept;
     ~PixelBitmap()
     {
@@ -28,27 +30,46 @@ public:
         }
     }
 
-    inline PixelBitmap& operator=(PixelBitmap&& moved_bitmap) noexcept
+    PixelBitmap& operator=(PixelBitmap& bitmap)
     {
-        m_data = moved_bitmap.m_data;
-        m_width = moved_bitmap.m_width;
-        m_height = moved_bitmap.m_height;
-        m_should_free = moved_bitmap.m_should_free;
+        m_bounds = bitmap.bounds();
+        m_should_free = bitmap.m_should_free;
+        if (m_should_free) {
+            size_t len = width() * height() * sizeof(Color);
+            m_data = (Color*)malloc(len);
+            memcpy((uint8_t*)m_data, (uint8_t*)bitmap.m_data, len);
+        } else {
+            m_data = bitmap.m_data;
+        }
         return *this;
     }
 
-    inline size_t width() const { return m_width; }
-    inline size_t height() const { return m_height; }
+    inline PixelBitmap& operator=(PixelBitmap&& moved_bitmap) noexcept
+    {
+        m_data = moved_bitmap.m_data;
+        m_bounds = moved_bitmap.bounds();
+        m_should_free = moved_bitmap.m_should_free;
+        moved_bitmap.m_data = nullptr;
+        moved_bitmap.bounds().set_width(0);
+        moved_bitmap.bounds().set_height(0);
+        moved_bitmap.m_should_free = false;
+        return *this;
+    }
+
+    inline size_t width() const { return m_bounds.width(); }
+    inline size_t height() const { return m_bounds.height(); }
+    inline LG::Rect& bounds() { return m_bounds; }
+    inline const LG::Rect& bounds() const { return m_bounds; }
     inline Color* data() const { return m_data; }
     inline Color* line(size_t i) { return m_data + i * width(); }
     inline const Color* line(size_t i) const { return m_data + i * width(); }
     inline Color* operator[](size_t i) { return line(i); }
     inline const Color* operator[](size_t i) const { return line(i); }
+    void resize(size_t width, size_t height);
 
 private:
     Color* m_data { nullptr };
-    size_t m_width { 0 };
-    size_t m_height { 0 };
+    LG::Rect m_bounds { 0, 0, 0, 0 };
     bool m_should_free { false };
 };
 
