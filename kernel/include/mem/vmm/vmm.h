b4a5617719b21b9c19544f5251312a671f9ab867
nimelehin <nimelehin@gmail.com>
2021-03-13 21:22:05 +0300

    [kernel][vmm] Add vmm_prepare_active_pdir_for_copying_at

--- kernel/include/mem/vmm/vmm.h
+++ kernel/include/mem/vmm/vmm.h
@@ -74,6 +74,7 @@ int vmm_copy_page(uint32_t to_vaddr, uint32_t src_vaddr, ptable_t* src_ptable);
 pdirectory_t* vmm_new_user_pdir();
 pdirectory_t* vmm_new_forked_user_pdir();
 void* vmm_bring_to_kernel(uint8_t* src, uint32_t length);
+void vmm_prepare_active_pdir_for_copying_at(uint32_t dest_vaddr, uint32_t length);
 void vmm_fast_copy_to_active_pdir(void* src, uint32_t dest_vaddr, uint32_t length);
 void vmm_copy_to_user(void* dest, void* src, uint32_t length);
 void vmm_copy_to_pdir(pdirectory_t* pdir, void* src, uint32_t dest_vaddr, uint32_t length);
--- kernel/kernel/mem/vmm/vmm.c
+++ kernel/kernel/mem/vmm/vmm.c
@@ -932,6 +932,11 @@ void vmm_fast_copy_to_active_pdir(void* src, uint32_t dest_vaddr, uint32_t lengt
     memcpy(dest, src, length);
 }
 
+void vmm_prepare_active_pdir_for_copying_at(uint32_t dest_vaddr, uint32_t length)
+{
+    _vmm_ensure_cow_for_range(dest_vaddr, length);
+}
+
 void vmm_copy_to_user(void* dest, void* src, uint32_t length)
 {
     _vmm_ensure_cow_for_range((uint32_t)dest, length);
