bf027aefedc54c61585122cc91238084e135a2b3
nimelehin <nimelehin@gmail.com>
2021-03-21 19:46:16 +0300

    [libfoundation] Add fast sqrt

---
+++ libs/libfoundation/include/libfoundation/Math.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#pragma once
+
+#include <cstddef>
+
+namespace LFoundation {
+
+[[gnu::always_inline]] inline float fast_inv_sqrt(float x)
+{
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;
+    i = 0x5f3759df - (i >> 1);
+    x = *(float*)&i;
+    x = x * (1.5f - xhalf * x * x);
+    return x;
+}
+
+[[gnu::always_inline]] inline float fast_sqrt(float x)
+{
+    return 1.0 / fast_inv_sqrt(x);
+}
+
+} // namespace LFoundation
