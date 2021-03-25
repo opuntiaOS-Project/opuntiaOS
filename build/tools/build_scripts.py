21cea87ef1e56e7ea1cd75e06882629f02d150bc
nimelehin <nimelehin@gmail.com>
2021-03-25 22:57:42 +0300

    [build] Add qemu env vars: ONEOS_QEMU_{ARCH}

--- build/tools/build_scripts.py
+++ build/tools/build_scripts.py
@@ -3,18 +3,25 @@
 # found in the LICENSE file.
 
 import sys
-QEMU_PATH = ""
+import os
+QEMU_PATH_VAR = "qemu_exec"
+QEMU_ENV_VAR = ""
+QEMU_STD_PATH = ""
 qemu_run_cmd = ""
 arch = sys.argv[1]
 base = sys.argv[2]
 out = sys.argv[3]
 
 if arch == "x86":
-    QEMU_PATH = "/Users/nikitamelehin/Develop/qemu-4.2.1/i386-softmmu/qemu-system-i386"
-    qemu_run_cmd =  "{2} -m 256M -fda {1}/os-image.bin -device piix3-ide,id=ide -drive id=disk,file={1}/one.img,if=none -device ide-drive,drive=disk,bus=ide.0 -serial mon:stdio -rtc base=utc -vga std".format(base, out, QEMU_PATH)
+    QEMU_ENV_VAR = "ONEOS_QEMU_X86"
+    QEMU_STD_PATH = "qemu-system-i386"
+    qemu_run_cmd = "${2} -m 256M -fda {1}/os-image.bin -device piix3-ide,id=ide -drive id=disk,file={1}/one.img,if=none -device ide-drive,drive=disk,bus=ide.0 -serial mon:stdio -rtc base=utc -vga std".format(
+        base, out, QEMU_PATH_VAR)
 if arch == "aarch32":
-    QEMU_PATH = "qemu-system-arm"
-    qemu_run_cmd =  "{2} -M vexpress-a15 -cpu cortex-a15 -kernel {1}/base/boot/kernel.bin -serial mon:stdio -vga std -sd {1}/one.img".format(base, out, QEMU_PATH)
+    QEMU_ENV_VAR = "ONEOS_QEMU_ARM"
+    QEMU_STD_PATH = "qemu-system-arm"
+    qemu_run_cmd = "${2} -M vexpress-a15 -cpu cortex-a15 -kernel {1}/base/boot/kernel.bin -serial mon:stdio -vga std -sd {1}/one.img".format(
+        base, out, QEMU_PATH_VAR)
 
 if base[-1] == '/':
     base = base[:-1]
@@ -24,7 +31,7 @@ if out[-1] == '/':
 
 sync = open("{0}/sync.sh".format(out), "w")
 sync.write(
-"""#!/bin/bash
+    """#!/bin/bash
 GREEN='\\033[0;32m'
 RED='\\033[0;31m'
 NC='\\033[0m'
@@ -44,13 +51,13 @@ sudo cp -r {1}/base/* {0}/mountpoint/
 sudo umount {0}/mountpoint
 if [ $? -ne 0 ]; then echo -e "${{ERROR}} Can't umount {0}/mountpoint" && exit 1; fi
 echo -e "${{SUCCESS}} Sync"
-""".format(base, out)) 
+""".format(base, out))
 sync.close()
 
 
 build = open("{0}/build.sh".format(out), "w")
 build.write(
-"""#!/bin/bash
+    """#!/bin/bash
 GREEN='\\033[0;32m'
 RED='\\033[0;31m'
 NC='\\033[0m'
@@ -60,28 +67,32 @@ SUCCESS="${{GREEN}}[SUCCESS]${{NC}}"
 ninja
 if [ $? -ne 0 ]; then echo -e "${{ERROR}} Can't build for arch: {0}" && exit 1; fi
 echo -e "${{SUCCESS}} Build for arch: {0}"
-""".format(arch)) 
+""".format(arch))
 build.close()
 
 run = open("{0}/run.sh".format(out), "w")
 run.write(
-"""#!/bin/bash
+    """#!/bin/bash
+{2}="{3}"
+[[ -z "${1}" ]] && {2}='{3}' || {2}="${1}"
 {0}
-if [ $? -ne 0 ]; then echo -e "${{ERROR}} Run command failed" && exit 1; fi""".format(qemu_run_cmd)
-) 
+if [ $? -ne 0 ]; then echo -e "${{ERROR}} Run command failed" && exit 1; fi""".format(qemu_run_cmd, QEMU_ENV_VAR, QEMU_PATH_VAR, QEMU_STD_PATH)
+)
 run.close()
 
 debug = open("{0}/debug.sh".format(out), "w")
 debug.write(
-"""#!/bin/bash
+    """#!/bin/bash
+{2}="{3}"
+[[ -z "${1}" ]] && {2}='{3}' || {2}="${1}"
 {0} -s -S
-if [ $? -ne 0 ]; then echo -e "${{ERROR}} Debug Run command failed" && exit 1; fi""".format(qemu_run_cmd)
-) 
+if [ $? -ne 0 ]; then echo -e "${{ERROR}} Debug Run command failed" && exit 1; fi""".format(qemu_run_cmd, QEMU_ENV_VAR, QEMU_PATH_VAR, QEMU_STD_PATH)
+)
 debug.close()
 
 allf = open("{0}/all.sh".format(out), "w")
 allf.write(
-"""#!/bin/bash
+    """#!/bin/bash
 GREEN='\\033[0;32m'
 RED='\\033[0;31m'
 NC='\\033[0m'
@@ -99,7 +110,7 @@ allf.close()
 
 allf = open("{0}/dll.sh".format(out), "w")
 allf.write(
-"""#!/bin/bash
+    """#!/bin/bash
 GREEN='\\033[0;32m'
 RED='\\033[0;31m'
 NC='\\033[0m'
@@ -112,11 +123,11 @@ if [ $? -ne 0 ]; then echo -e "${ERROR} Debug All command failed" && exit 1; fi
 if [ $? -ne 0 ]; then echo -e "${ERROR} Debug All command failed" && exit 1; fi
 ./debug.sh
 if [ $? -ne 0 ]; then echo -e "${ERROR} Debug All command failed" && exit 1; fi
-""") 
+""")
 allf.close()
 
 gdbinit = open("{0}/.gdbinit".format(out), "w")
 gdbinit.write(
-"""file {0}/base/boot/kernel.bin
+    """file {0}/base/boot/kernel.bin
 target remote :1234""".format(out))
 gdbinit.close()
