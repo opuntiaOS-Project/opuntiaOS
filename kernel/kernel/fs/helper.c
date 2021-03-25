487469c97108ba9dfb793243d70496e367a934fc
nimelehin <nimelehin@gmail.com>
2021-03-25 22:05:58 +0300

    [kernel][vfs] Align vfs_helper_write_dirent

--- kernel/kernel/fs/helper.c
+++ kernel/kernel/fs/helper.c
@@ -11,20 +11,23 @@
 #include <libkern/log.h>
 #include <tasking/tasking.h>
 
+#define NORM_FILENAME(x) ((x + 0x3) & (uint32_t)(~0b11))
+
 ssize_t vfs_helper_write_dirent(dirent_t* buf, uint32_t buf_len, uint32_t inode_index, const char* name)
 {
     uint32_t name_len = strlen(name);
-    int rec_len = 8 + name_len + 1;
+    int phys_rec_len = 8 + NORM_FILENAME(name_len) + 1;
+    int virt_rec_len = 8 + name_len + 1;
 
-    if (buf_len < rec_len) {
+    if (buf_len < phys_rec_len) {
         return -ENOMEM;
     }
 
     buf->inode = inode_index;
-    buf->rec_len = rec_len;
+    buf->rec_len = phys_rec_len;
     buf->name_len = name_len;
     uint8_t* buf_u8 = (uint8_t*)buf;
     memcpy(buf_u8 + 8, name, name_len);
-    buf_u8[rec_len - 1] = '\0';
-    return rec_len;
+    buf_u8[virt_rec_len - 1] = '\0';
+    return phys_rec_len;
 }
