9e7e50f0f7da4357e9a5026ea4571c2e23e8269b
nimelehin <nimelehin@gmail.com>
2021-04-01 14:00:54 +0300

    [kernel] Fix missing _start for kernel

--- boot/x86/stage3_entry.s
+++ boot/x86/stage3_entry.s
@@ -1,7 +1,9 @@
 section .oneos_kernel_boot
 
 [bits 32]
-[extern stage3]
-push dword [esp+4]
-call stage3
-jmp $
+extern stage3
+global _start
+_start:
+    push dword [esp+4]
+    call stage3
+    jmp $
