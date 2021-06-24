071bf21a51ff3b422afa508a7395cc7d1c094d9c
nimelehin <nimelehin@gmail.com>
2021-06-24 22:24:03 +0300

    [build] Use info.json from applications/

--- build/userland/prepare_app.py
+++ build/userland/prepare_app.py
@@ -7,17 +7,24 @@ import os
 import json
 import subprocess
 
-outpath = sys.argv[1]
-app_name = sys.argv[2]
+app_name = sys.argv[1]
+outpath = sys.argv[2]
+src_dir = sys.argv[3]
 
 
 def print_json(config_file, rdict):
     json.dump(rdict, config_file, indent=4)
 
 
-def write_config(outpath):
+def read_config(path):
+    with open(path) as json_file:
+        data = json.load(json_file)
+        return data
+    return {}
+
+
+def write_config(config, outpath):
     config_file = open(outpath+"/info.json", "w")
-    config = {}
 
     config['name'] = app_name
 
@@ -27,4 +34,11 @@ def write_config(outpath):
 
 if not os.path.exists(outpath):
     os.makedirs(outpath)
-write_config(outpath)
+
+config = {}
+for fname in os.listdir(src_dir):
+    if fname == "info.json":
+        config = read_config(src_dir + "/info.json")
+        break
+
+write_config(config, outpath)
