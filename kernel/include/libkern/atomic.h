13161ffa7002c857f343cce9bfeef9bff760e9dd
nimelehin <nimelehin@gmail.com>
2021-06-28 20:35:19 +0300

    [kernel][libkern] Add atomics

---
+++ kernel/include/libkern/atomic.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _KERNEL_LIBKERN_ATOMIC_H
+#define _KERNEL_LIBKERN_ATOMIC_H
+
+#include <libkern/c_attrs.h>
+#include <libkern/kassert.h>
+#include <libkern/types.h>
+
+static ALWAYS_INLINE int atomic_add_int(int* dst, int val)
+{
+    return __atomic_add_fetch(dst, val, __ATOMIC_SEQ_CST) + val;
+}
+
+static ALWAYS_INLINE uint32_t atomic_add_uint32(uint32_t* dst, uint32_t val)
+{
+    return __atomic_add_fetch(dst, val, __ATOMIC_SEQ_CST) + val;
+}
+
+#endif // _KERNEL_LIBKERN_LOCK_H
