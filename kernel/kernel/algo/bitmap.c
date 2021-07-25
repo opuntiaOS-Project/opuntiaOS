01ceb5aa8ef18e3c7eb64cc5683d039c9d32099d
nimelehin <nimelehin@gmail.com>
2021-07-25 10:15:14 +0300

    [kernel][algo] Optimize Bitmap range accesses

--- kernel/kernel/algo/bitmap.c
+++ kernel/kernel/algo/bitmap.c
@@ -9,6 +9,8 @@
 #include <libkern/bits/errno.h>
 #include <mem/kmalloc.h>
 
+#define BITMAP_BLOCKS_PER_BYTE (8)
+
 bitmap_t bitmap_wrap(uint8_t* data, uint32_t len)
 {
     bitmap_t bitmap;
@@ -20,8 +22,8 @@ bitmap_t bitmap_wrap(uint8_t* data, uint32_t len)
 /* FIXME: Let user know if alloction was unsucessful */
 bitmap_t bitmap_allocate(uint32_t len)
 {
-    uint32_t alloc_len = len / 8;
-    if (len % 8) {
+    uint32_t alloc_len = len / BITMAP_BLOCKS_PER_BYTE;
+    if (len % BITMAP_BLOCKS_PER_BYTE) {
         alloc_len++;
     }
     uint8_t* data = kmalloc(alloc_len);
@@ -38,12 +40,12 @@ int bitmap_find_space(bitmap_t bitmap, int req)
             taken = 0;
             continue;
         }
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < BITMAP_BLOCKS_PER_BYTE; j++) {
             if ((bitmap.data[i] >> j) & 1) {
                 taken = 0;
             } else {
                 if (taken == 0) {
-                    start = i * 8 + j;
+                    start = i * BITMAP_BLOCKS_PER_BYTE + j;
                 }
                 taken++;
                 if (taken == req) {
@@ -64,12 +66,12 @@ int bitmap_find_space_aligned(bitmap_t bitmap, int req, int alignment)
             taken = 0;
             continue;
         }
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < BITMAP_BLOCKS_PER_BYTE; j++) {
             if ((bitmap.data[i] >> j) & 1) {
                 taken = 0;
             } else {
                 if (taken == 0) {
-                    start = i * 8 + j;
+                    start = i * BITMAP_BLOCKS_PER_BYTE + j;
                 }
                 if ((start % alignment) == 0) {
                     taken++;
@@ -85,12 +87,12 @@ int bitmap_find_space_aligned(bitmap_t bitmap, int req, int alignment)
 
 int bitmap_set(bitmap_t bitmap, int where)
 {
-    if (where >= bitmap.len * 8) {
+    if (where >= bitmap.len * BITMAP_BLOCKS_PER_BYTE) {
         return -EFAULT;
     }
 
-    int block = where / 8;
-    int offset = where % 8;
+    int block = where / BITMAP_BLOCKS_PER_BYTE;
+    int offset = where % BITMAP_BLOCKS_PER_BYTE;
     bitmap.data[block] |= (1 << offset);
 
     return 0;
@@ -98,12 +100,12 @@ int bitmap_set(bitmap_t bitmap, int where)
 
 int bitmap_unset(bitmap_t bitmap, int where)
 {
-    if (where >= bitmap.len * 8) {
+    if (where >= bitmap.len * BITMAP_BLOCKS_PER_BYTE) {
         return -EFAULT;
     }
 
-    int block = where / 8;
-    int offset = where % 8;
+    int block = where / BITMAP_BLOCKS_PER_BYTE;
+    int offset = where % BITMAP_BLOCKS_PER_BYTE;
     bitmap.data[block] &= ~((1 << offset));
 
     return 0;
@@ -111,20 +113,60 @@ int bitmap_unset(bitmap_t bitmap, int where)
 
 int bitmap_set_range(bitmap_t bitmap, int start, int len)
 {
-    for (int i = start; i < start + len; i++) {
-        if (bitmap_set(bitmap, i) != 0) {
-            return -EPERM;
-        }
+    if (start + len - 1 >= bitmap.len * BITMAP_BLOCKS_PER_BYTE) {
+        return -EFAULT;
     }
+
+    int where = start;
+
+    while (len && (where % BITMAP_BLOCKS_PER_BYTE) != 0) {
+        bitmap_set(bitmap, where);
+        where++;
+        len--;
+    }
+
+    while (len >= BITMAP_BLOCKS_PER_BYTE) {
+        int block = where / BITMAP_BLOCKS_PER_BYTE;
+        bitmap.data[block] = 0xff;
+        where += BITMAP_BLOCKS_PER_BYTE;
+        len -= BITMAP_BLOCKS_PER_BYTE;
+    }
+
+    while (len) {
+        bitmap_set(bitmap, where);
+        where++;
+        len--;
+    }
+
     return 0;
 }
 
 int bitmap_unset_range(bitmap_t bitmap, int start, int len)
 {
-    for (int i = start; i < start + len; i++) {
-        if (bitmap_unset(bitmap, i) != 0) {
-            return -EPERM;
-        }
+    if (start + len - 1 >= bitmap.len * BITMAP_BLOCKS_PER_BYTE) {
+        return -EFAULT;
     }
+
+    int where = start;
+
+    while (len && (where % BITMAP_BLOCKS_PER_BYTE) != 0) {
+        bitmap_unset(bitmap, where);
+        where++;
+        len--;
+    }
+
+    while (len >= BITMAP_BLOCKS_PER_BYTE) {
+        int block = where / BITMAP_BLOCKS_PER_BYTE;
+        bitmap.data[block] = 0x0;
+        where += BITMAP_BLOCKS_PER_BYTE;
+        len -= BITMAP_BLOCKS_PER_BYTE;
+    }
+
+    while (len) {
+        bitmap_unset(bitmap, where);
+        where++;
+        len--;
+    }
+
     return 0;
 }
