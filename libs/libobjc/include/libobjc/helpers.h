5ced92c58c17a2417c7440732d8f9371a3799bd0
nimelehin <nimelehin@gmail.com>
2021-08-19 19:46:37 +0300

    [libobjc] Init of Objective-C runtime

---
+++ libs/libobjc/include/libobjc/helpers.h
@@ -0,0 +1,6 @@
+#ifndef _LIBOBJC_HELPERS_H
+#define _LIBOBJC_HELPERS_H
+
+#define OBJC_EXPORT extern "C"
+
+#endif // _LIBOBJC_HELPERS_H
---
+++ libs/libobjc/include/libobjc/selector.h
@@ -0,0 +1,19 @@
+#ifndef _LIBOBJC_SELECTOR_H
+#define _LIBOBJC_SELECTOR_H
+
+#include <stddef.h>
+
+struct objc_selector {
+    void* id;
+};
+typedef struct objc_selector* SEL;
+
+static inline bool sel_equal(SEL s1, SEL s2)
+{
+    if (s1 == NULL || s2 == NULL) {
+        return s1 == s2;
+    }
+    return s1->id == s2->id;
+}
+
+#endif // _LIBOBJC_SELECTOR_H
