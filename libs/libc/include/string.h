adc9810e52cb84e3ec0bec586bb00b15bfb69994
bellrise <65973858+bellrise@users.noreply.github.com>
2021-08-23 18:09:56 +0200

    [libc][string] Optimize memcpy
    The memcpy function now copies 4 bytes at a time on the 32 bit target
    architecture. This speeds up the process for larger chunks of data
    by approx. 4 times. More information on this function can be found
    in the comment about the memcpy implementation.

--- libs/libc/include/string.h
+++ libs/libc/include/string.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Modified by: bellrise
+ */
 #ifndef _LIBC_STRING_H
 #define _LIBC_STRING_H
 
@@ -7,15 +15,37 @@
 
 __BEGIN_DECLS
 
-void* memset(void* dest, int fll, size_t nbytes);
-void* memmove(void* dest, const void* src, size_t nbytes);
-void* memcpy(void* dest, const void* src, size_t nbytes);
-void* memccpy(void* dest, const void* src, int stop, size_t nbytes);
+/* Set 'nbytes' to 'fill' starting from 'dest'. */
+void* memset(void* dest, int fill, size_t nbytes);
+
+/* Move 'nbytes' from 'src' to 'dest' */
+void* memmove(void* dest, const void* __restrict src, size_t nbytes);
+
+/* Copy 'nbytes' from 'src' to 'dest'. See the comment in the source file
+   about optimization and restricting pointers. */
+void* memcpy(void* __restrict dest, const void* __restrict src, size_t nbytes);
+
+/* Copy 'nbytes' from 'src' to 'dest', stopping if the current byte matches
+   'stop'. Note that the stop byte also gets copied over. */
+void* memccpy(void* dest, const void* __restrict src, int stop, size_t nbytes);
+
+/* Compare 'nbytes' from 'src1' and 'src2'. Return 0 if all the bytes match,
+   otherwise return the difference. */
 int memcmp(const void* src1, const void* src2, size_t nbytes);
 
-size_t strlen(const char* s);
+/* Calculate the string length starting from 'str'. */
+size_t strlen(const char* str);
+
+/* Copy 'src' into 'dest' until it finds a null byte in the source string.
+   Note that this is dangerous because it writes memory no matter the size
+   the 'dest' buffer is. */
 char* strcpy(char* dest, const char* src);
-char* strncpy(char* dest, const char* src, size_t n);
+
+/* Copy 'src' into 'dest' until it finds a null byte or reaches the 'nbytes'
+   limit provided by the user. This is the recommended way of copying strings,
+   instead of using regular strcpy. Note that this will fill the 'dest' buffer
+   with null bytes if the amount of copied bytes is lower than 'nbytes'. */
+char* strncpy(char* dest, const char* src, size_t nbytes);
 
 __END_DECLS
 
