7ad3ddee5b103b444f0a53fb693b35f7d1ea30e2
bellrise <65973858+bellrise@users.noreply.github.com>
2021-08-24 14:02:15 +0200

    [libs][stdio] Refractor stdio.c
    Added some comments, cleaned up the code, moved all static functions
    to the bottom of the file, leaving the public ones at the top.

--- libs/libc/stdio/stdio.c
+++ libs/libc/stdio/stdio.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Modified by: bellrise
+ */
+#include <bits/fcntl.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -29,340 +38,49 @@ FILE* stdin = &_stdstreams[0];
 FILE* stdout = &_stdstreams[1];
 FILE* stderr = &_stdstreams[2];
 
-static size_t _do_system_write(const void* ptr, size_t size, FILE* stream);
+/* Static functions */
+static inline int _can_read(FILE *file);
+static inline int _can_write(FILE *file);
+static inline int _can_use_buffer(FILE *file);
+static int _parse_mode(const char* mode, mode_t* flags);
 
+/* Buffer */
+static inline int _free_buf(FILE *stream);
+static size_t _do_system_write(const void* ptr, size_t size, FILE* stream);
 static int _resize_buf(FILE* stream, size_t size);
 static ssize_t _flush_wbuf(FILE* stream);
+static void _split_rwbuf(FILE* stream);
+static int _resize_buf(FILE* stream, size_t size);
 
-/**
- * FLAGS
- */
+/* Stream */
+static int _init_stream(FILE* file);
+static int _init_file_with_fd(FILE* file, int fd);
+static int _open_file(FILE* file, const char* path, const char* mode);
+static FILE* _fopen_internal(const char* path, const char* mode);
 
-inline static int _can_read(FILE* file)
-{
-    return ((file->_flags & _IO_NO_READS) == 0);
-}
+/* Read/write */
+static size_t _do_system_read(char* ptr, size_t size, FILE* stream);
+static size_t _do_system_write(const void* ptr, size_t size, FILE* stream);
+static size_t _fread_internal(char* ptr, size_t size, FILE* stream);
+static size_t _fwrite_internal(const void* ptr, size_t size, FILE* stream);
 
-inline static int _can_write(FILE* file)
-{
-    return ((file->_flags & _IO_NO_WRITES) == 0);
-}
 
-inline static int _can_use_buffer(FILE* file)
-{
-    return ((file->_flags & _IO_UNBUFFERED) == 0);
-}
-
-/**
- * BUFFER
- */
-
-static inline void _init_buf(FILE* stream)
-{
-    _resize_buf(stream, BUFSIZ);
-}
-
-static inline int _free_buf(FILE* stream)
-{
-    if (!(stream->_flags & _IO_USER_BUF) && stream->_bf.base) {
-        free(stream->_bf.base);
-    }
-    return 0;
-}
-
-/**
- * _split_rwbuf splits newly allocated buffer to a read buffer
- * and a write buffer.
- */
-static void _split_rwbuf(FILE* stream)
-{
-    // TODO: Base on stream flags.
-    stream->_bf.rbuf.base = stream->_bf.base;
-    stream->_bf.rbuf.ptr = stream->_bf.rbuf.base;
-    stream->_bf.rbuf.size = ((stream->_bf.size + 1) / 2) & ((size_t) ~(0b11));
-
-    stream->_bf.wbuf.base = stream->_bf.base + stream->_bf.rbuf.size;
-    stream->_bf.wbuf.ptr = stream->_bf.wbuf.base;
-    stream->_bf.wbuf.size = (stream->_bf.size - stream->_bf.rbuf.size) & ((size_t) ~(0b11));
-
-    stream->_r = 0;
-    stream->_w = stream->_bf.wbuf.size;
-}
-
-/**
- * _resize_buf frees prev buffer and allocates new with size.
- */
-static int _resize_buf(FILE* stream, size_t size)
-{
-    _free_buf(stream);
-
-    if (!size) {
-        return 0;
-    }
-
-    stream->_bf.base = malloc(size);
-    if (!stream->_bf.base) {
-        stream->_r = 0;
-        stream->_w = 0;
-        return -1;
-    }
-
-    stream->_bf.size = size;
-    _split_rwbuf(stream);
-    return 0;
-}
-
-static int _set_buf(FILE* stream, char* buf, int mode, size_t size)
-{
-    stream->_flags &= ~(int)(_IO_UNBUFFERED | _IO_LINE_BUF);
-    if (mode & _IOLBF) {
-        stream->_flags |= _IO_LINE_BUF;
-    }
-    if (mode & _IONBF) {
-        stream->_flags |= _IO_UNBUFFERED;
-    }
-
-    _flush_wbuf(stream);
-
-    if (!_can_use_buffer(stream)) {
-        return _free_buf(stream);
-    }
-
-    if (!buf) {
-        return _resize_buf(stream, size);
-    }
-
-    stream->_bf.base = buf;
-    stream->_bf.size = size;
-    _split_rwbuf(stream);
-    return 0;
-}
-
-static ssize_t _flush_wbuf(FILE* stream)
-{
-    size_t write_size = stream->_bf.wbuf.size - stream->_w;
-    size_t written = _do_system_write(stream->_bf.wbuf.base, write_size, stream);
-    if (written != write_size) {
-        return -EFAULT;
-    }
-    stream->_w = stream->_bf.wbuf.size;
-    stream->_bf.wbuf.ptr = stream->_bf.wbuf.base;
-    return (ssize_t)write;
-}
-
-static int _parse_mode(const char* mode, mode_t* flags)
-{
-    *flags = 0;
-    int len = strlen(mode);
-    if (!len) {
-        return 0;
-    }
-    // TODO: Add binary mode
-    int has_plus = 0;
-    for (int i = 0; i < len; i++) {
-        if (mode[i] == '+') {
-            has_plus = 1;
-        }
-    }
-
-    switch (mode[0]) {
-    case 'r':
-        if (has_plus) {
-            *flags = O_RDWR;
-        } else {
-            *flags = O_RDONLY;
-        }
-        return 0;
-
-    case 'w':
-        if (has_plus) {
-            *flags = O_RDWR | O_CREAT;
-        } else {
-            *flags = O_WRONLY | O_CREAT;
-        }
-        return 0;
-
-    case 'a':
-        if (has_plus) {
-            *flags = O_APPEND | O_CREAT;
-        } else {
-            *flags = O_APPEND | O_CREAT;
-        }
-        return 0;
-
-    default:
-        return -1;
-    }
-
-    return -1;
-}
-
-/**
- * STREAM
- */
-
-static inline int _init_stream(FILE* file)
-{
-    file->_file = -1;
-    file->_flags = _IO_MAGIC;
-    file->_r = 0;
-    file->_w = 0;
-    file->_bf.base = NULL;
-    file->_bf.size = 0;
-    file->_ungotc = UNGOTC_EMPTY;
-    return 0;
-}
-
-static int _init_file_with_fd(FILE* file, int fd)
-{
-    _init_stream(file);
-    _init_buf(file);
-    file->_file = fd;
-    return 0;
-}
-
-static int _open_file(FILE* file, const char* path, const char* mode)
-{
-    mode_t flags = 0;
-    int err = _parse_mode(mode, &flags);
-    if (err) {
-        return err;
-    }
-
-    int fd = open(path, flags);
-    if (fd < 0) {
-        return errno;
-    }
-
-    file->_file = fd;
-    return 0;
-}
-
-static FILE* _fopen_internal(const char* path, const char* mode)
-{
-    FILE* file = malloc(sizeof(FILE));
-    if (!file) {
-        return NULL;
-    }
-
-    _init_stream(file);
-    _init_buf(file);
-    _open_file(file, path, mode);
-    return file;
-}
+/* Public functions */
 
 FILE* fopen(const char* path, const char* mode)
 {
-    if (!path) {
+    if (!path || !mode)
         return NULL;
-    }
-    if (!mode) {
-        return NULL;
-    }
+
     return _fopen_internal(path, mode);
-}
-
-static size_t _do_system_read(char* ptr, size_t size, FILE* stream)
-{
-    ssize_t read_cnt = read(stream->_file, ptr, size);
-    if (read_cnt < 0) {
-        return 0;
-    }
-    return (size_t)read_cnt;
-}
-
-static size_t _fread_internal(char* ptr, size_t size, FILE* stream)
-{
-    if (size == 0) {
-        return 0;
-    }
-
-    if (!_can_use_buffer(stream)) {
-        return _do_system_read(ptr, size, stream);
-    }
-
-    size_t total_size = 0;
-
-    if (stream->_ungotc != UNGOTC_EMPTY) {
-        ptr[0] = (char)stream->_ungotc;
-        ptr++;
-        size--;
-        total_size++;
-        stream->_ungotc = UNGOTC_EMPTY;
-    }
-
-    if (stream->_r) {
-        size_t read_from_buf = min(stream->_r, size);
-        memcpy(ptr, stream->_bf.rbuf.ptr, read_from_buf);
-        ptr += read_from_buf;
-        size -= read_from_buf;
-        stream->_bf.rbuf.ptr += read_from_buf;
-        stream->_r -= read_from_buf;
-        total_size += read_from_buf;
-    }
-
-    while (size > 0) {
-        stream->_bf.rbuf.ptr = stream->_bf.rbuf.base;
-        stream->_r = _do_system_read(stream->_bf.rbuf.ptr, stream->_bf.rbuf.size, stream);
-
-        if (!stream->_r) {
-            return total_size;
-        }
-
-        size_t read_from_buf = min(stream->_r, size);
-        memcpy(ptr, stream->_bf.rbuf.ptr, read_from_buf);
-        ptr += read_from_buf;
-        size -= read_from_buf;
-        stream->_bf.rbuf.ptr += read_from_buf;
-        stream->_r -= read_from_buf;
-        total_size += read_from_buf;
-    }
-
-    return total_size;
 }
 
 size_t fread(void* ptr, size_t size, size_t count, FILE* stream)
 {
-    if (!ptr) {
+    if (!ptr || !stream)
         return 0;
-    }
-    if (!stream) {
-        return 0;
-    }
+
     return _fread_internal(ptr, size * count, stream);
-}
-
-static size_t _do_system_write(const void* ptr, size_t size, FILE* stream)
-{
-    ssize_t write_cnt = write(stream->_file, ptr, size);
-    if (write_cnt < 0) {
-        return 0;
-    }
-    return (size_t)write_cnt;
-}
-
-static size_t _fwrite_internal(const void* ptr, size_t size, FILE* stream)
-{
-    if (!_can_use_buffer(stream)) {
-        return _do_system_write(ptr, size, stream);
-    }
-
-    size_t total_size = 0;
-
-    while (size > 0) {
-        size_t write_size = min(stream->_w, size);
-        memcpy(stream->_bf.wbuf.ptr, ptr, write_size);
-        ptr += write_size;
-        size -= write_size;
-        stream->_bf.wbuf.ptr += write_size;
-        stream->_w -= write_size;
-        total_size += write_size;
-        if (!stream->_w) {
-            _flush_wbuf(stream);
-        }
-    }
-
-    return total_size;
 }
 
 size_t fwrite(const void* ptr, size_t size, size_t count, FILE* stream)
@@ -371,24 +89,23 @@ size_t fwrite(const void* ptr, size_t size, size_t count, FILE* stream)
         set_errno(EINVAL);
         return 0;
     }
+
     if (!stream) {
         set_errno(EINVAL);
         return 0;
     }
+
     return _fwrite_internal(ptr, size * count, stream);
 }
 
-int fseek(FILE* stream, uint32_t offset, int whence)
-{
-    return -1;
-}
+/* TODO: Implement fseek */
 
 int fputc(int c, FILE* stream)
 {
     int res = fwrite(&c, 1, 1, stream);
-    if (!res) {
+    if (!res)
         return -errno;
-    }
+
     return c;
 }
 
@@ -404,11 +121,14 @@ int putchar(int c)
 
 int fputs(const char* s, FILE* stream)
 {
+    // HERE
     size_t len = strlen(s);
+
     int res = fwrite(s, len, 1, stream);
-    if (!res) {
+
+    if (!res)
         return -errno;
-    }
+
     return res;
 }
 
@@ -420,10 +140,10 @@ int puts(const char* s)
 int fgetc(FILE* stream)
 {
     char c;
-    size_t res = fread(&c, 1, 1, stream);
-    if (res != 1) {
+
+    if (fread(&c, 1, 1, stream) != 1)
         return EOF;
-    }
+
     return c;
 }
 
@@ -439,9 +159,8 @@ int getchar()
 
 int ungetc(int c, FILE* stream)
 {
-    if (c == EOF) {
+    if (c == EOF)
         return EOF;
-    }
 
     if (!stream) {
         set_errno(EINVAL);
@@ -464,8 +183,7 @@ char* fgets(char* s, int size, FILE* stream)
         return NULL;
     }
 
-    size_t count = fread(s, size, 1, stream);
-    return count ? s : NULL;
+    return fread(s, size, 1, stream) ? s : NULL;
 }
 
 int setvbuf(FILE* stream, char* buf, int mode, size_t size)
@@ -480,7 +198,28 @@ int setvbuf(FILE* stream, char* buf, int mode, size_t size)
         return -1;
     }
 
-    return _set_buf(stream, buf, mode, size);
+    /* Clear the buffer type flags and reset it. */
+
+    stream->_flags &= ~(int)(_IO_UNBUFFERED | _IO_LINE_BUF);
+    if (mode & _IOLBF)
+        stream->_flags |= _IO_LINE_BUF;
+
+    if (mode & _IONBF)
+        stream->_flags |= _IO_UNBUFFERED;
+
+    _flush_wbuf(stream);
+
+    if (!_can_use_buffer(stream))
+        return _free_buf(stream);
+
+    if (!buf)
+        return _resize_buf(stream, size);
+
+    stream->_bf.base = buf;
+    stream->_bf.size = size;
+    _split_rwbuf(stream);
+
+    return 0;
 }
 
 void setbuf(FILE* stream, char* buf)
@@ -495,18 +234,17 @@ void setlinebuf(FILE* stream)
 
 int fflush(FILE* stream)
 {
-    if (!stream) {
+    if (!stream)
         return -EBADF;
-    }
 
     return _flush_wbuf(stream);
 }
 
 int _stdio_init()
 {
-    _init_file_with_fd(stdin, 0);
-    _init_file_with_fd(stdout, 1);
-    _init_file_with_fd(stderr, 2);
+    _init_file_with_fd(stdin, STDIN);
+    _init_file_with_fd(stdout, STDOUT);
+    _init_file_with_fd(stderr, STDERR);
     setbuf(stderr, NULL);
     return 0;
 }
@@ -516,4 +254,272 @@ int _stdio_deinit()
     // FIXME
     _flush_wbuf(stdout);
     return 0;
+}
+
+/* Static functions */
+
+static inline int _can_read(FILE* file)
+{
+    return (file->_flags & _IO_NO_READS) == 0;
+}
+
+static inline int _can_write(FILE* file)
+{
+    return (file->_flags & _IO_NO_WRITES) == 0;
+}
+
+static inline int _can_use_buffer(FILE* file)
+{
+    return (file->_flags & _IO_UNBUFFERED) == 0;
+}
+
+/* Because this checks the first and second character only, the possible
+   combinations are: r, w, a, r+ and w+. */
+static int _parse_mode(const char* mode, mode_t* flags)
+{
+    int has_plus, len;
+
+    if (!(len = strlen(mode)))
+        return 0;
+
+    *flags = 0;
+    if (len > 1 && mode[1] == '+')
+        has_plus = 1;
+
+    switch (mode[0]) {
+    case 'r':
+        *flags = has_plus ? O_RDWR : O_RDONLY;
+        return 0;
+
+    case 'w':
+        *flags = has_plus ? O_RDWR | O_CREAT : O_WRONLY | O_CREAT;
+        return 0;
+
+    case 'a':
+        *flags = O_APPEND | O_CREAT;
+        return 0;
+
+    /* TODO: Add binary mode when the rest will support such option. */
+
+    default:
+        return -1;
+    }
+
+    return -1;
+}
+
+/* Buffer */
+
+static inline int _free_buf(FILE* stream)
+{
+    /* Don't free the buffer if the user provided one with setvbuf. */
+    if (stream->_flags & _IO_USER_BUF)
+        return 0;
+
+    if (stream->_bf.base)
+        free(stream->_bf.base);
+
+    return 0;
+}
+
+static void _split_rwbuf(FILE* stream)
+{
+    size_t rsize, wsize;
+
+    rsize = ((stream->_bf.size + 1) / 2) & (size_t) ~0x03;
+    wsize = (stream->_bf.size - rsize) & (size_t) ~0x03;
+
+    /* TODO: Base on stream flags. */
+    stream->_bf.rbuf.base = stream->_bf.base;
+    stream->_bf.rbuf.ptr = stream->_bf.rbuf.base;
+    stream->_bf.rbuf.size = rsize;
+
+    stream->_bf.wbuf.base = stream->_bf.base + stream->_bf.rbuf.size;
+    stream->_bf.wbuf.ptr = stream->_bf.wbuf.base;
+    stream->_bf.wbuf.size = wsize;
+
+    stream->_r = 0;
+    stream->_w = stream->_bf.wbuf.size;
+}
+
+static int _resize_buf(FILE* stream, size_t size)
+{
+    _free_buf(stream);
+
+    if (!size)
+        return 0;
+
+    stream->_bf.base = malloc(size);
+
+    if (!stream->_bf.base) {
+        stream->_r = 0;
+        stream->_w = 0;
+        return -1;
+    }
+
+    stream->_bf.size = size;
+    _split_rwbuf(stream);
+
+    return 0;
+}
+
+static ssize_t _flush_wbuf(FILE* stream)
+{
+    size_t write_size, written;
+
+    write_size = stream->_bf.wbuf.size - stream->_w;
+    written = _do_system_write(stream->_bf.wbuf.base, write_size, stream);
+
+    if (written != write_size)
+        return -EFAULT;
+
+    stream->_w = stream->_bf.wbuf.size;
+    stream->_bf.wbuf.ptr = stream->_bf.wbuf.base;
+    return (ssize_t)write;
+}
+
+/* Stream */
+
+static int _init_stream(FILE* file)
+{
+    file->_file = -1;
+    file->_flags = _IO_MAGIC;
+    file->_r = 0;
+    file->_w = 0;
+    file->_bf.base = NULL;
+    file->_bf.size = 0;
+    file->_ungotc = UNGOTC_EMPTY;
+    return 0;
+}
+
+static int _init_file_with_fd(FILE* file, int fd)
+{
+    _init_stream(file);
+    _resize_buf(file, BUFSIZ);
+    file->_file = fd;
+    return 0;
+}
+
+static int _open_file(FILE* file, const char* path, const char* mode)
+{
+    mode_t flags = 0;
+    int err = _parse_mode(mode, &flags);
+
+    if (err)
+        return err;
+
+    int fd = open(path, flags);
+    if (fd < 0)
+        return errno;
+
+    file->_file = fd;
+    return 0;
+}
+
+static FILE* _fopen_internal(const char* path, const char* mode)
+{
+    FILE *file = malloc(sizeof(FILE));
+    if (!file)
+        return NULL;
+
+    _init_stream(file);
+    _resize_buf(file, BUFSIZ);
+    _open_file(file, path, mode);
+    return file;
+}
+
+/* Read */
+
+static size_t _do_system_read(char* ptr, size_t size, FILE* stream)
+{
+    ssize_t read_size = read(stream->_file, ptr, size);
+    return read_size < 0 ? 0 : (size_t) read_size;
+}
+
+static size_t _do_system_write(const void* ptr, size_t size, FILE* stream)
+{
+    ssize_t write_size = write(stream->_file, ptr, size);
+
+    if (write_size < 0)
+        return 0;
+
+    return (size_t) write_size;
+}
+
+static size_t _fread_internal(char* ptr, size_t size, FILE* stream)
+{
+    size_t total_size, read_from_buf;
+
+    if (!size)
+        return 0;
+
+    if (!_can_use_buffer(stream))
+        return _do_system_read(ptr, size, stream);
+
+    total_size = 0;
+
+    /* If the ungot char buffer is not empty, push it onto the buffer first. */
+    if (stream->_ungotc != UNGOTC_EMPTY) {
+        ptr[0] = (char)stream->_ungotc;
+        ptr++;
+        size--;
+        total_size++;
+        stream->_ungotc = UNGOTC_EMPTY;
+    }
+
+    /* First read any bytes still sitting in the read buffer. */
+    if (stream->_r) {
+        read_from_buf = min(stream->_r, size);
+        memcpy(ptr, stream->_bf.rbuf.ptr, read_from_buf);
+        ptr += read_from_buf;
+        size -= read_from_buf;
+        stream->_bf.rbuf.ptr += read_from_buf;
+        stream->_r -= read_from_buf;
+        total_size += read_from_buf;
+    }
+
+    /* Read the remaining bytes that were not stored in the read buffer. */
+    while (size > 0) {
+        stream->_bf.rbuf.ptr = stream->_bf.rbuf.base;
+        stream->_r = _do_system_read(
+            stream->_bf.rbuf.ptr, stream->_bf.rbuf.size, stream
+        );
+
+        if (!stream->_r)
+            return total_size;
+
+        read_from_buf = min(stream->_r, size);
+        memcpy(ptr, stream->_bf.rbuf.ptr, read_from_buf);
+        ptr += read_from_buf;
+        size -= read_from_buf;
+        stream->_bf.rbuf.ptr += read_from_buf;
+        stream->_r -= read_from_buf;
+        total_size += read_from_buf;
+    }
+
+    return total_size;
+}
+
+static size_t _fwrite_internal(const void* ptr, size_t size, FILE* stream)
+{
+    size_t total_size;
+
+    if (!_can_use_buffer(stream))
+        return _do_system_write(ptr, size, stream);
+
+    total_size = 0;
+    while (size > 0) {
+        size_t write_size = min(stream->_w, size);
+        memcpy(stream->_bf.wbuf.ptr, ptr, write_size);
+        ptr += write_size;
+        size -= write_size;
+        stream->_bf.wbuf.ptr += write_size;
+        stream->_w -= write_size;
+        total_size += write_size;
+
+        if (!stream->_w)
+            _flush_wbuf(stream);
+    }
+
+    return total_size;
 }
