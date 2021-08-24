48c4ea4bdda9a68f71881e07a8941cd84477877a
bellrise <65973858+bellrise@users.noreply.github.com>
2021-08-24 12:42:15 +0200

    [libc][cdefs] Provide __use_instead macro
    You can mark functions that have better alternatives with this macro.
    For example, this is used in the stdio for getc, which it marked with
    __use_instead("fgetc").

--- libs/libc/include/sys/cdefs.h
+++ libs/libc/include/sys/cdefs.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Modified by: bellrise
+ */
 #ifndef _LIBC_SYS_CDEFS_H
 #define _LIBC_SYS_CDEFS_H
 
@@ -7,6 +15,17 @@
 #else
 #define __BEGIN_DECLS
 #define __END_DECLS
+#endif
+
+/* Define __use_instead macro for some functions so the user can be warned
+   about better/faster functions. */
+#ifndef __use_instead
+# ifdef __clang__
+#  define __use_instead(F) __attribute__((diagnose_if(1, "use " F " instead", \
+            "warning")))
+# elif defined(__GNUC__)
+#  define __use_instead(F) __attribute__((warning("use " F " instead")))
+# endif
 #endif
 
 #endif // _LIBC_SYS_CDEFS_H
