1f621d415449ac565bdd26d1cf6e71b06f6eeeaa
bellrise <65973858+bellrise@users.noreply.github.com>
2021-08-24 12:43:51 +0200

    [libc][stdio] Comment functions and add STD macros
    Add multiple comments for each function in stdio.h and define 3 new
    macros: STDIN, STDOUT and STDERR.

--- libs/libc/include/stdio.h
+++ libs/libc/include/stdio.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2020-2021 Nikita Melekhin. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Modified by: bellrise
+ */
 #ifndef _LIBC_STDIO_H
 #define _LIBC_STDIO_H
 
@@ -14,6 +22,12 @@ __BEGIN_DECLS
 #define EOF (-1)
 #define UNGOTC_EMPTY (-0x400)
 
+/* Define stdin, stdout and stderr macros for better readability. */
+#define STDIN   0
+#define STDOUT  1
+#define STDERR  2
+
+/* Single read/write buffer. */
 struct __fbuf {
     char* base;
     char* ptr; /* current pointer */
@@ -21,6 +35,7 @@ struct __fbuf {
 };
 typedef struct __fbuf __fbuf_t;
 
+/* Read/write buffer. */
 struct __rwbuf {
     __fbuf_t rbuf;
     __fbuf_t wbuf;
@@ -29,6 +44,7 @@ struct __rwbuf {
 };
 typedef struct __rwbuf __rwbuf_t;
 
+/* Structure representing a file. */
 struct __file {
     int _flags; /* flags, below; this FILE is free if 0 */
     int _file; /* fileno, if Unix descriptor, else -1 */
@@ -43,41 +59,74 @@ extern FILE* stdin;
 extern FILE* stdout;
 extern FILE* stderr;
 
+/* Open a file. Returns a struct representing an open file allocated on the
+   heap. The possible mode combinations are: r, w, a, r+ and w+. Returns NULL
+   if anything goes wrong. */
 FILE* fopen(const char* filename, const char* mode);
+
+/* Deallocate the file and close it. */
 int fclose(FILE* stream);
 
-size_t fread(void* ptr, size_t size, size_t nmemb, FILE* stream);
-size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream);
-int fseek(FILE* stream, uint32_t offset, int whence);
+/* Read up to 'count' objects, each of 'size' size into 'buf' from 'stream' */
+size_t fread(void* buf, size_t size, size_t count, FILE* stream);
+
+/* Write up to 'count' objects, each of 'size' size into 'stream' from 'buf'. */
+size_t fwrite(const void* buf, size_t size, size_t count, FILE* stream);
+
+/* Set the file indicator to 'offset', starting from 'origin'. */
+int fseek(FILE* stream, uint32_t offset, int origin);
+
+/* Flush all buffers, writing all stored data into the stored device/file. */
 int fflush(FILE* stream);
 
+/* Put a single character to the given stream. 'putchar' will write the char
+   to stdout. */
 int fputc(int c, FILE* stream);
-int putc(int c, FILE* stream);
+int putc(int c, FILE* stream) __use_instead("fputc");
 int putchar(int c);
 
+/* Put a string to the given stream with a newline at the end, 'puts' writes
+   to stdout by default. */
 int fputs(const char* s, FILE* stream);
 int puts(const char* s);
 
+/* Read/place back a char from the given stream. 'getchar' will get a single
+   char from stdin. */
 int fgetc(FILE* stream);
-int getc(FILE* stream);
+int getc(FILE* stream) __use_instead("fgets");
 int getchar();
 int ungetc(int c, FILE* stream);
 
-char* fgets(char* s, int size, FILE* stream);
+/* Get a string from the given stream, of max 'size'. */
+char* fgets(char* str, int size, FILE* stream);
 
+/* Set the buffering mode of the stream. 'mode' can either be _IOFBF, _IOLBF
+   or _IONBF. If 'buf' is NULL, resize the internal buffer to 'size'. If 'buf'
+   is instead not NULL, it changes the internal buffer to the user provided
+   one of size 'size'. Return -1 if something goes wrong. */
 int setvbuf(FILE* stream, char* buf, int mode, size_t size);
+
+/* Set the buffering mode to full buffering if 'buf' is provided. The minimum
+   size of the buffer should be BUFSIZ, or it could lead to a buffer overflow.
+   If 'buf' is NULL, buffering it turned off. */
 void setbuf(FILE* stream, char* buf);
+
+/* Set the buffering mode of the given stream to _IOLBF. */
 void setlinebuf(FILE* stream);
 
-int vsnprintf(char* s, size_t n, const char* format, va_list arg);
-int vsprintf(char* s, const char* format, va_list arg);
-int snprintf(char* s, size_t n, const char* format, ...);
-int sprintf(char* s, const char* format, ...);
-int fprintf(FILE* stream, const char* format, ...);
-int printf(const char* format, ...);
+/* Works the same as the C standard says, check there. */
+int vsnprintf(char* buf, size_t n, const char* fmt, va_list arg);
+int vsprintf(char* buf, const char* fmt, va_list arg);
+int snprintf(char* buf, size_t n, const char* fmt, ...);
+int sprintf(char* buf, const char* fmt, ...);
+int fprintf(FILE* stream, const char* fmt, ...);
+int printf(const char* fmt, ...);
 
-int vsscanf(const char* buf, const char* format, va_list arg);
-int sscanf(const char* str, const char* fmt, ...);
+/* Scan a buffer for the provided values. Works similarly to printf, but it
+   reads instead of writes. 'scanf' reads from stdin instead of a buffer. Note
+   that using these functions may be unsafe. Never trust the user! */
+int vsscanf(const char* buf, const char* fmt, va_list arg);
+int sscanf(const char* buf, const char* fmt, ...);
 int scanf(const char* fmt, ...);
 
 __END_DECLS
