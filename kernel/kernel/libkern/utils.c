b81f12d8faa1c78cac7ca68ba217184bf2faeb48
nimelehin <nimelehin@gmail.com>
2021-08-26 19:21:30 +0300

    [kernel][libkern] Fix strcmp routines

--- kernel/kernel/libkern/utils.c
+++ kernel/kernel/libkern/utils.c
@@ -86,10 +86,10 @@ int strcmp(const char* a, const char* b)
         b++;
     }
 
-    if (a < b) {
+    if (*a < *b) {
         return -1;
     }
-    if (a > b) {
+    if (*a > *b) {
         return 1;
     }
     return 0;
@@ -107,10 +107,10 @@ int strncmp(const char* a, const char* b, uint32_t num)
         return 0;
     }
 
-    if (a < b) {
+    if (*a < *b) {
         return -1;
     }
-    if (a > b) {
+    if (*a > *b) {
         return 1;
     }
     return 0;
