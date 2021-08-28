13392277328b223b25109b836fce7b3a85e59378
nimelehin <nimelehin@gmail.com>
2021-08-28 18:29:34 +0300

    [libobjc] Support args passing in messages

--- libs/libobjc/include/libobjc/runtime.h
+++ libs/libobjc/include/libobjc/runtime.h
@@ -1,9 +1,12 @@
 #ifndef _LIBOBJC_RUNTIME_H
 #define _LIBOBJC_RUNTIME_H
 
+#include <libobjc/helpers.h>
 #include <libobjc/isa.h>
 #include <libobjc/selector.h>
 #include <libobjc/v1/decls.h>
+
+OBJC_EXPORT void objc_msgSend(id reciever, SEL sel, ...);
 
 static inline Class object_getClass(id object)
 {
