8266bd0acb815468f608c415797532a5640e563f
nimelehin <nimelehin@gmail.com>
2021-09-09 19:55:07 +0300

    [kernel][libkern] Optimize bitmap

--- kernel/kernel/algo/bitmap.c
+++ kernel/kernel/algo/bitmap.c
@@ -8,9 +8,12 @@
 
 #include <algo/bitmap.h>
 #include <libkern/bits/errno.h>
+#include <libkern/platform.h>
 #include <mem/kmalloc.h>
 
 #define BITMAP_BLOCKS_PER_BYTE (8)
+
+#define bitmap_get(bitmap, where) ((bitmap.data[where / BITMAP_BLOCKS_PER_BYTE] >> (where % BITMAP_BLOCKS_PER_BYTE)) & 1)
 
 bitmap_t bitmap_wrap(uint8_t* data, uint32_t len)
 {
@@ -31,31 +34,106 @@ bitmap_t bitmap_allocate(uint32_t len)
     return bitmap_wrap(data, len);
 }
 
-/* TODO: can be speeded up with tzcnt */
-int bitmap_find_space(bitmap_t bitmap, int req)
+int bitmap_next_range_of_unset_bits(bitmap_t bitmap, int from, size_t min_len, int* start_of_free_chunks)
 {
-    int taken = 0;
-    int start = 0;
-    for (int i = 0; i < bitmap.len; i++) {
-        if (bitmap.data[i] == 0xff) {
-            taken = 0;
+    uint32_t* bitmap32 = (uint32_t*)bitmap.data;
+
+    // Calculating the start offset.
+    int start_bucket_index = from / 32;
+    int start_bucket_bit = from % 32;
+
+    size_t free_chunks = 0;
+
+    for (size_t bucket_index = start_bucket_index; bucket_index < bitmap.len / 32; bucket_index++) {
+        if (bitmap32[bucket_index] == 0xffffffff) {
+            // Skip over completely full bucket of size 32.
+            if (free_chunks >= min_len) {
+                return free_chunks;
+            }
+            free_chunks = 0;
+            start_bucket_bit = 0;
             continue;
         }
-        for (int j = 0; j < BITMAP_BLOCKS_PER_BYTE; j++) {
-            if ((bitmap.data[i] >> j) & 1) {
-                taken = 0;
+        if (bitmap32[bucket_index] == 0x0) {
+            // Skip over completely empty bucket of size 32.
+            if (free_chunks == 0) {
+                *start_of_free_chunks = bucket_index * 32;
+            }
+            free_chunks += 32;
+            if (free_chunks >= min_len) {
+                return free_chunks;
+            }
+            start_bucket_bit = 0;
+            continue;
+        }
+
+        uint32_t bucket = bitmap32[bucket_index];
+        int viewed_bits = start_bucket_bit;
+        uint32_t trailing_zeroes = 0;
+
+        bucket >>= viewed_bits;
+        start_bucket_bit = 0;
+
+        while (viewed_bits < 32) {
+            if (bucket == 0) {
+                if (free_chunks == 0) {
+                    *start_of_free_chunks = bucket_index * 32 + viewed_bits;
+                }
+                free_chunks += 32 - viewed_bits;
+                viewed_bits = 32;
             } else {
-                if (taken == 0) {
-                    start = i * BITMAP_BLOCKS_PER_BYTE + j;
+                trailing_zeroes = ctz32(bucket);
+                bucket >>= trailing_zeroes;
+
+                if (free_chunks == 0) {
+                    *start_of_free_chunks = bucket_index * 32 + viewed_bits;
                 }
-                taken++;
-                if (taken == req) {
-                    return start;
+                free_chunks += trailing_zeroes;
+                viewed_bits += trailing_zeroes;
+
+                if (free_chunks >= min_len) {
+                    return free_chunks;
                 }
+
+                // Deleting trailing ones.
+                uint32_t trailing_ones = ctz32(~bucket);
+                bucket >>= trailing_ones;
+                viewed_bits += trailing_ones;
+                free_chunks = 0;
             }
         }
     }
-    return -ENODATA;
+
+    if (free_chunks < min_len) {
+        size_t first_trailing_bit = (bitmap.len / 32) * 32;
+        size_t trailing_bits = bitmap.len % 32;
+        for (size_t i = 0; i < trailing_bits; i++) {
+            if (!bitmap_get(bitmap, first_trailing_bit + i)) {
+                if (!free_chunks) {
+                    *start_of_free_chunks = first_trailing_bit + i;
+                }
+                if (++free_chunks >= min_len) {
+                    return free_chunks;
+                }
+            } else {
+                free_chunks = 0;
+            }
+        }
+        return -1;
+    }
+
+    return free_chunks;
+}
+
+int bitmap_find_space(bitmap_t bitmap, int req)
+{
+    int start = 0;
+    int len = bitmap_next_range_of_unset_bits(bitmap, 0, req, &start);
+    if (len < 0) {
+        return -ENODATA;
+    }
+
+    return start;
 }
 
 int bitmap_find_space_aligned(bitmap_t bitmap, int req, int alignment)
