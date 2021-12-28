5a91db0ca0ad8b5a6be798a5788aab2a0b065580
nimelehin <nimelehin@gmail.com>
2021-12-28 20:05:42 +0300

    [kernel][kswapd] Update algorithm of finding victims

--- kernel/kernel/mem/kswapd.c
+++ kernel/kernel/mem/kswapd.c
@@ -16,10 +16,15 @@
 #include <tasking/tasking.h>
 
 // #define KSWAPD_DEBUG
-#define KSWAPD_SLEEPTIME (10) // seconds.
+#define KSWAPD_SLEEPTIME (3) // seconds.
+#define KSWAPD_SWAP_PER_PID_THRESHOLD (2)
+#define KSWAPD_SWAP_PER_RUN_THRESHOLD (4)
 
 static kmemzone_t _mapzone;
 static uintptr_t _mmaped_ptable;
+static int moved_out_pages_per_run = 0;
+static int moved_out_pages_per_pid = 0;
+static int last_pid = 0, last_pti = 0;
 extern proc_t proc[MAX_PROCESS_COUNT];
 
 static int map_ptable(table_desc_t* ptable_desc)
@@ -38,6 +43,18 @@ static int map_ptable(table_desc_t* ptable_desc)
     return 0;
 }
 
+inline static void after_page_swap()
+{
+    moved_out_pages_per_run++;
+    moved_out_pages_per_pid++;
+}
+
+static void do_sleep()
+{
+    moved_out_pages_per_run = 0;
+    ksys1(SYS_NANOSLEEP, KSWAPD_SLEEPTIME);
+}
+
 static int find_victim(proc_t* p, pdirectory_t* pdir)
 {
     lock_acquire(&p->vm_lock);
@@ -45,7 +62,7 @@ static int find_victim(proc_t* p, pdirectory_t* pdir)
     const size_t ptables_per_page = VMM_PAGE_SIZE / PTABLE_SIZE;
     const size_t table_coverage = VMM_PAGE_SIZE * VMM_TOTAL_PAGES_PER_TABLE;
 
-    for (int pti = 0; pti < VMM_KERNEL_TABLES_START; pti++) {
+    for (int pti = last_pti; pti < VMM_KERNEL_TABLES_START; pti++, last_pti++) {
         table_desc_t* ptable_desc = &pdir->entities[pti];
         if (!table_desc_has_attrs(*ptable_desc, TABLE_DESC_PRESENT)) {
             continue;
@@ -63,8 +80,11 @@ static int find_victim(proc_t* p, pdirectory_t* pdir)
             log("[kswapd] (pid %d) Find victim at %x", p->pid, victim_vaddr);
 #endif
             vmm_swap_page(&ptable_map_zone[pti], NULL, victim_vaddr);
-            lock_release(&p->vm_lock);
-            return 0;
+            after_page_swap();
+            if (moved_out_pages_per_pid >= KSWAPD_SWAP_PER_PID_THRESHOLD) {
+                lock_release(&p->vm_lock);
+                return 0;
+            }
         }
     }
 
@@ -85,14 +105,20 @@ void kswapd()
         }
 
         proc_t* p;
-        for (int i = 0; i < tasking_get_proc_count(); i++) {
+        for (int i = last_pid; i < tasking_get_proc_count(); i++, last_pid++) {
             p = &proc[i];
             if (p->status == PROC_ALIVE) {
                 find_victim(p, p->pdir);
             }
+            last_pti = 0;
+            moved_out_pages_per_pid = 0;
+            if (moved_out_pages_per_run >= KSWAPD_SWAP_PER_RUN_THRESHOLD) {
+                goto sleep;
+            }
         }
+        last_pid = 0;
 
     sleep:
-        ksys1(SYS_NANOSLEEP, KSWAPD_SLEEPTIME);
+        do_sleep();
     }
 }
