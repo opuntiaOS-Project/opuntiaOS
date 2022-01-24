16bf37a1b48a342fbd3065fd2a60d3f1fa44f605
nimelehin <nimelehin@gmail.com>
2022-01-24 22:26:30 +0300

    [kernel][platform] Use __builtin_ctz for aarch32

--- kernel/include/libkern/platform.h
+++ kernel/include/libkern/platform.h
@@ -13,16 +13,7 @@
 
 ALWAYS_INLINE int ctz32(unsigned int val)
 {
-#if defined(__GNUC__) || defined(__clang__)
     return __builtin_ctz(val);
-#else
-    for (u8 i = 0; i < 32; ++i) {
-        if ((val >> i) & 1) {
-            return i;
-        }
-    }
-    return 0;
-#endif
 }
 
 #endif // _KERNEL_LIBKERN_PLATFORM_H
