d9145d607ddc09bfff766896c5dd08cddf1cfb95
nimelehin <nimelehin@gmail.com>
2022-03-13 10:58:18 +0300

    [userland][init] Implement init in C and signed

--- userland/system/init/.info.mk
+++
@@ -1,5 +0,0 @@
-APPS += INIT
-
-INIT_NAME = init
-INIT_LIBS = c
-INIT_INSTALL_PATH = boot/
---
+++ userland/system/init/main.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+#define initsystem_path "/System/initsystem"
+
+int main() 
+{
+    execlp(initsystem_path, initsystem_path, NULL);
+    return 0;
+}
