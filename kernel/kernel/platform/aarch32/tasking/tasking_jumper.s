6c9042153f344a3b58b64b61d36a3d02f22144a5
nimelehin <nimelehin@gmail.com>
2022-03-29 22:33:23 +0300

    [kernel][arm] Set cpu state when switching to userspace

--- kernel/kernel/platform/aarch32/tasking/tasking_jumper.s
+++ kernel/kernel/platform/aarch32/tasking/tasking_jumper.s
@@ -1,9 +1,11 @@
 .global _tasking_jumper
 .global switch_to_context
 .extern system_enable_interrupts
+.extern _asm_cpu_enter_user_space
 
 // void _tasking_jumper()
 _tasking_jumper:
+    bl      _asm_cpu_enter_user_space
     bl      system_enable_interrupts_only_counter
     mov     r0, sp // saving current sp to bring it to the new mode
     cps	    #0x13
---
+++ kernel/kernel/tasking/cpu.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2020-2022 The opuntiaOS Project Authors.
+ *  + Contributed by Nikita Melekhin <nimelehin@gmail.com>
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <tasking/cpu.h>
+
+void _asm_cpu_enter_user_space()
+{
+    cpu_enter_user_space();
+}
