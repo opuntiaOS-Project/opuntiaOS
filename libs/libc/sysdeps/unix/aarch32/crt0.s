f626e9d4776b15846214f67960300136e3bd872e
nimelehin <nimelehin@gmail.com>
2022-03-29 22:31:34 +0300

    [libc][arm] Fix stack alignment in crt0

--- libs/libc/sysdeps/unix/aarch32/crt0.s
+++ libs/libc/sysdeps/unix/aarch32/crt0.s
@@ -7,11 +7,15 @@
 
 .global _start
 _start:
-	push {r0-r2}
-	bl _init
-	pop {r0-r2}
-	bl main
-	push {r0}
-	bl _deinit
-	pop {r0}
-	bl exit
+	push    {r0-r2}
+	sub     sp, sp, #4 // align to 16bytes
+	bl      _init
+	add     sp, sp, #4
+	pop     {r0-r2}
+	bl      main
+	push    {r0}
+	sub     sp, sp, #12 // align to 16bytes
+	bl      _deinit
+	add     sp, sp, #12
+	pop     {r0}
+	bl      exit
