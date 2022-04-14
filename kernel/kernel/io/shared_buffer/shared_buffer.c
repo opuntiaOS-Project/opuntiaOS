4e13c9ce91748ba372a10bafcf75f808d96cdaab
nimelehin <nimelehin@gmail.com>
2022-04-14 22:54:08 +0300

    [kernel] Validate user accessing shared_buffer

--- kernel/kernel/io/shared_buffer/shared_buffer.c
+++ kernel/kernel/io/shared_buffer/shared_buffer.c
@@ -14,6 +14,7 @@
 #include <libkern/log.h>
 #include <mem/kmemzone.h>
 #include <mem/vmm.h>
+#include <tasking/tasking.h>
 
 // #define SHARED_BUFFER_DEBUG
 
@@ -21,7 +22,13 @@
 #define SHBUF_BLOCK_SIZE (4 * KB)
 #define SHBUF_MAX_BUFFERS 128
 
-uint8_t* buffers[SHBUF_MAX_BUFFERS];
+struct buffer_desc {
+    uint8_t* data;
+    uid_t owner_uid;
+};
+typedef struct buffer_desc buffer_desc_t;
+
+buffer_desc_t buffer_descs[SHBUF_MAX_BUFFERS];
 
 struct shared_buffer_header {
     size_t len;
@@ -47,7 +54,7 @@ static inline int _shared_buffer_to_index(uintptr_t vaddr)
 static inline int _shared_buffer_alloc_id()
 {
     for (int i = 0; i < SHBUF_MAX_BUFFERS; i++) {
-        if (!buffers[i]) {
+        if (!buffer_descs[i].data) {
             return i;
         }
     }
@@ -99,8 +106,10 @@ int shared_buffer_create(uintptr_t __user* res_buffer, size_t size)
     vmm_tune_pages((uintptr_t)space, act_size, MMU_FLAG_PERM_WRITE | MMU_FLAG_PERM_EXEC | MMU_FLAG_PERM_READ | MMU_FLAG_NONPRIV);
 
     uintptr_t result_pointer = (uintptr_t)&space[1];
+    buffer_descs[buf_id].data = (uint8_t*)result_pointer;
+    buffer_descs[buf_id].owner_uid = RUNNING_THREAD->process->pid;
     umem_put_user(result_pointer, res_buffer);
-    buffers[buf_id] = (uint8_t*)&space[1];
+
 #ifdef SHARED_BUFFER_DEBUG
     log("Buffer created at %x %d", buffers[buf_id], buf_id);
 #endif
@@ -115,14 +124,20 @@ int shared_buffer_get(int id, uintptr_t __user* res_buffer)
     }
 
     spinlock_acquire(&_shared_buffer_lock);
-    if (unlikely(buffers[id] == 0)) {
+    if (unlikely(!buffer_descs[id].data)) {
         spinlock_release(&_shared_buffer_lock);
         return -EINVAL;
     }
+
+    if (!proc_is_su(RUNNING_THREAD->process) && RUNNING_THREAD->process->pid != buffer_descs[id].owner_uid) {
+        spinlock_release(&_shared_buffer_lock);
+        return -EPERM;
+    }
+
 #ifdef SHARED_BUFFER_DEBUG
-    log("Buffer opened at %x %d", buffers[id], id);
+    log("Buffer opened at %x %d", buffer_descs[id].data, id);
 #endif
-    uintptr_t result_pointer = (uintptr_t)buffers[id];
+    uintptr_t result_pointer = (uintptr_t)buffer_descs[id].data;
     umem_put_user(result_pointer, res_buffer);
     spinlock_release(&_shared_buffer_lock);
     return 0;
@@ -135,15 +150,20 @@ int shared_buffer_free(int id)
     }
 
     spinlock_acquire(&_shared_buffer_lock);
-    if (unlikely(buffers[id] == 0)) {
+    if (unlikely(!buffer_descs[id].data)) {
         spinlock_release(&_shared_buffer_lock);
         return -EINVAL;
     }
 
-    shared_buffer_header_t* sptr = (shared_buffer_header_t*)buffers[id];
+    if (!proc_is_su(RUNNING_THREAD->process) && RUNNING_THREAD->process->pid != buffer_descs[id].owner_uid) {
+        spinlock_release(&_shared_buffer_lock);
+        return -EPERM;
+    }
+
+    shared_buffer_header_t* sptr = (shared_buffer_header_t*)buffer_descs[id].data;
     size_t blocks_to_delete = (sptr[-1].len + SHBUF_BLOCK_SIZE - 1) / SHBUF_BLOCK_SIZE;
     bitmap_unset_range(bitmap, _shared_buffer_to_index((uintptr_t)&sptr[-1]), blocks_to_delete);
-    buffers[id] = 0;
+    buffer_descs[id].data = NULL;
     spinlock_release(&_shared_buffer_lock);
     return 0;
 }
