bd26bd84c2611e93d720905ab65a07d9da7c0232
nimelehin <nimelehin@gmail.com>
2022-04-21 22:54:29 +0300

    [utils] Set gnu98 in clang-tidy to mute memcpy_s like warnings
    Mute them for now, will support in the future.

--- utils/codeassistant/clang_tidy.py
+++ utils/codeassistant/clang_tidy.py
@@ -82,7 +82,7 @@ class ClassTidyLauncher():
         self.process_includes()
         self.get_files()
         self.c_back_flags = self.back_flags
-        self.c_back_flags += ["-std=gnu11"]
+        self.c_back_flags += ["-std=gnu98"]
         ret = ""
         if len(self.c_files) > 0:
             ret += self.run_clang_tidy(self.front_flags,
