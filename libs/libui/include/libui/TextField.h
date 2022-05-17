e2b0e028d0f7755ccd0adc33381b0edeca4c8a0f
nimelehin <nimelehin@gmail.com>
2022-05-17 19:51:18 +0300

    [libui] Init of TextField

--- libs/libui/include/libui/TextField.h
+++ libs/libui/include/libui/TextField.h
@@ -26,21 +26,21 @@ public:
 
     ~TextField() = default;
 
-    const std::string& title() const { return m_title; }
-    void set_title(const std::string& title) { m_title = title, recalc_bounds(), set_needs_display(); }
-    void set_title(std::string&& title) { m_title = std::move(title), recalc_bounds(), set_needs_display(); }
+    const std::string& text() const { return m_text; }
+    void set_text(const std::string& text) { m_text = text, set_needs_display(); }
+    void set_text(std::string&& text) { m_text = std::move(text), set_needs_display(); }
 
-    void set_title_color(const LG::Color& color) { m_title_color = color; }
-    const LG::Color& title_color() const { return m_title_color; }
+    void set_placeholder_text(const std::string& text) { m_placeholder_text = text, set_needs_display(); }
+    void set_placeholder_text(std::string&& text) { m_placeholder_text = std::move(text), set_needs_display(); }
+
+    void set_text_color(const LG::Color& color) { m_text_color = color; }
+    const LG::Color& text_color() const { return m_text_color; }
 
     void set_content_edge_insets(const EdgeInsets& ei) { m_content_edge_insets = ei; }
     const EdgeInsets& content_edge_insets() const { return m_content_edge_insets; }
 
-    void set_font(const LG::Font& font) { m_font = font, recalc_bounds(); }
+    void set_font(const LG::Font& font) { m_font = font, set_needs_display(); }
     inline const LG::Font& font() const { return m_font; }
-
-    void set_alignment(Text::Alignment alignment) { m_alignment = alignment; }
-    Text::Alignment alignment() const { return m_alignment; }
 
     virtual void display(const LG::Rect& rect) override;
     virtual void mouse_entered(const LG::Point<int>& location) override;
@@ -53,14 +53,12 @@ protected:
     TextField(View* superview, const LG::Rect& frame);
 
 private:
-    void recalc_bounds();
-    size_t text_height() const;
-    size_t text_width();
+    size_t text_width(const std::string& text);
+    size_t text_height(const std::string& text) const;
 
-    Type m_button_type { Type::System };
-
-    std::string m_title {};
-    LG::Color m_title_color { LG::Color::White };
+    std::string m_text {};
+    std::string m_placeholder_text {};
+    LG::Color m_text_color { LG::Color::DarkSystemText };
     LG::Font m_font { LG::Font::system_font() };
 
     Text::Alignment m_alignment { Text::Alignment::Left };
