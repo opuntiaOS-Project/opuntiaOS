bbc1670d1f56683f5809036030b28600da105fef
nimelehin <nimelehin@gmail.com>
2022-05-18 07:57:01 +0300

    [test] Add script to run tests on all platforms

---
+++ test/all_platforms.sh
@@ -0,0 +1,28 @@
+# Run from the root dir.
+
+gn clean out
+./gn_gen.sh --target_cpu x86 --host llvm --test_method tests
+cd out
+./build.sh
+./sync.sh
+python3 ../utils/test/test.py
+cd ..
+gn clean out
+
+gn clean out
+./gn_gen.sh --target_cpu arm --host llvm --test_method tests
+cd out
+./build.sh
+./sync.sh
+python3 ../utils/test/test.py
+cd ..
+gn clean out
+
+gn clean out
+./gn_gen.sh --target_cpu aarch64 --target_board qemu_virt --host llvm --test_method tests
+cd out
+./build.sh
+./sync.sh
+python3 ../utils/test/test.py
+cd ..
+gn clean out
