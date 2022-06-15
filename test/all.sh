c455cde1ef1bfa8a170666c47a0ec04b1cb7d02f
nimelehin <nimelehin@gmail.com>
2022-06-15 23:11:14 +0300

    [test] Use arch test scripts in all.sh

--- test/all.sh
+++ test/all.sh
@@ -1,28 +1,6 @@
 # Run from the root dir.
 
-gn clean out
-./gn_gen.sh --target_cpu x86 --host llvm --test_method tests
-cd out
-./build.sh
-./sync.sh
-python3 ../utils/test/test.py
-cd ..
-gn clean out
-
-gn clean out
-./gn_gen.sh --target_cpu arm --host llvm --test_method tests
-cd out
-./build.sh
-./sync.sh
-python3 ../utils/test/test.py
-cd ..
-gn clean out
-
-gn clean out
-./gn_gen.sh --target_cpu aarch64 --target_board qemu_virt --host gnu --test_method tests
-cd out
-./build.sh
-./sync.sh
-python3 ../utils/test/test.py
-cd ..
-gn clean out
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+$SCRIPT_DIR/x86.sh
+$SCRIPT_DIR/aarch64.sh
+$SCRIPT_DIR/arm.sh
