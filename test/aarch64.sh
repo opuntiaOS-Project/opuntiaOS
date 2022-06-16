d1d5239bdd2dd8ab2f3164d573c566787d17c77d
nimelehin <nimelehin@gmail.com>
2022-06-16 23:09:05 +0300

    [test] Test all platforms with both compilers

--- test/aarch64.sh
+++ test/aarch64.sh
@@ -8,3 +8,10 @@ cd out
 python3 ../utils/test/test.py
 cd ..
 gn clean out
+./gn_gen.sh --target_cpu aarch64 --target_board qemu_virt --host llvm --test_method tests
+cd out
+./build.sh
+./sync.sh
+python3 ../utils/test/test.py
+cd ..
+gn clean out
