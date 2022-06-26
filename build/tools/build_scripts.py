f4c355c9b930f32543843d5f32533660713052a0
nimelehin <nimelehin@gmail.com>
2022-06-26 22:20:33 +0300

    [x86_64] Init of prekernel

--- build/tools/build_scripts.py
+++ build/tools/build_scripts.py
@@ -20,6 +20,11 @@ if arch == "x86":
     QEMU_STD_PATH = "qemu-system-i386"
     qemu_run_cmd = "${2} -m 256M --drive file={1}/os-image.bin,format=raw,index=0,if=floppy -device piix3-ide,id=ide -drive id=disk,format=raw,file={1}/one.img,if=none -device ide-hd,drive=disk,bus=ide.0 -serial mon:stdio -rtc base=utc -vga std -qmp unix:./qmp-sock,server,nowait".format(
         base, out, QEMU_PATH_VAR)
+if arch == "x86_64":
+    QEMU_PATH_ENV_VAR = "ONEOS_QEMU_X86_64"
+    QEMU_STD_PATH = "qemu-system-x86_64"
+    qemu_run_cmd = "${2} -m 256M -kernel {1}/prekernelx86_64.bin -hda {1}/one.img -serial mon:stdio -rtc base=utc -vga std".format(
+        base, out, QEMU_PATH_VAR)
 if arch == "arm32":
     QEMU_PATH_ENV_VAR = "ONEOS_QEMU_ARM"
     QEMU_STD_PATH = "qemu-system-arm"
