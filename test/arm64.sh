b7f7231731fb151437eddad65435915de3995a9a
nimelehin <nimelehin@gmail.com>
2022-07-17 21:51:56 +0300

    [ci] Add -y to gn_gen

--- test/arm64.sh
+++ test/arm64.sh
@@ -1,14 +1,14 @@
 # Run from the root dir.
 
 gn clean out
-./gn_gen.sh --target_arch arm64 --target_board qemu_virt --host gnu --test_method tests
+./gn_gen.sh -y --target_arch arm64 --target_board qemu_virt --host gnu --test_method tests
 cd out
 ./build.sh
 ./sync.sh
 python3 ../utils/test/test.py
 cd ..
 gn clean out
-./gn_gen.sh --target_arch arm64 --target_board qemu_virt --host llvm --test_method tests
+./gn_gen.sh -y --target_arch arm64 --target_board qemu_virt --host llvm --test_method tests
 cd out
 ./build.sh
 ./sync.sh
--- test/x86.sh
+++ test/x86.sh
@@ -1,14 +1,14 @@
 # Run from the root dir.
 
 gn clean out
-./gn_gen.sh --target_arch x86 --host gnu --test_method tests
+./gn_gen.sh -y --target_arch x86 --host gnu --test_method tests
 cd out
 ./build.sh
 ./sync.sh
 python3 ../utils/test/test.py
 cd ..
 gn clean out
-./gn_gen.sh --target_arch x86 --host llvm --test_method tests
+./gn_gen.sh -y --target_arch x86 --host llvm --test_method tests
 cd out
 ./build.sh
 ./sync.sh
