7a74e5d94b5715ca1e3cd81d8f980e042ed4def7
nimelehin <nimelehin@gmail.com>
2022-07-30 17:27:06 +0300

    [libcxx] Add std::type_info

---
+++ libs/libcxx/src/typeinfo.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+#include <typeinfo>
+
+_LIBCXX_BEGIN_NAMESPACE_STD
+
+bool type_info::operator==(const type_info& rhs) const
+{
+    // GCC has a convention where the typeinfo name for a class which
+    // is declared in an anonymous namespace begins with '*'.
+    if (name()[0] == '*' || rhs.name()[0] == '*') {
+        return false;
+    }
+    return this == &rhs || strcmp(name(), rhs.name()) == 0;
+}
+
+bool type_info::operator!=(const type_info& rhs) const
+{
+    return !operator==(rhs);
+}
+
+bool type_info::before(const type_info& rhs) const
+{
+    // GCC has a convention where the typeinfo name for a class which
+    // is declared in an anonymous namespace begins with '*'.
+    if (name()[0] == '*' && rhs.name()[0] == '*') {
+        return name() < rhs.name();
+    }
+    return strcmp(name(), rhs.name()) < 0;
+}
+
+_LIBCXX_END_NAMESPACE_STD
